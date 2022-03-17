cd56faabf828cc7b9c60d92e8f0f9e7f4c4b2d09
Varun Srinivasan <varunsrin@gmail.com>
2022-03-17 14:59:49 -0700

    feat: ping-pong nodes (#2)
    * fix: tsc should build with correct paths
    
    * create simple farcaster nodes that ping-pong each other

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import FarcasterNode, { InstanceName } from '~/farcasterNode';
+
+// 1. Create Farcaster Node instances and assign them into the Node Registry.
+const nodeRegistry = new Map<InstanceName, FarcasterNode>();
+
+for (const name of FarcasterNode.instanceNames) {
+  nodeRegistry.set(name, new FarcasterNode(name));
+}
+
+// 2. Give each Node instance a reference to the Registry so they can reach other nodes,
+// and make them ping each other at randomized intervals.
+for (const node of nodeRegistry.values()) {
+  node.setPeers(nodeRegistry);
+  setInterval(() => {
+    node.pingAll();
+  }, Math.floor(Math.random() * 30_000));
+}
