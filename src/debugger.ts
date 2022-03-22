f1309922044ae858943deae18e79a38f9a838a7e
Varun Srinivasan <varunsrin@gmail.com>
2022-03-21 19:14:44 -0700

    feat: add colors to mark latest chain

--- src/debugger.ts
+++ src/debugger.ts
@@ -1,19 +1,35 @@
 import FCNode, { NodeList } from '~/node';
 import { isCast, SignedCastChain } from '~/types';
 import Table from 'cli-table3';
+import colors from 'colors/safe';
 
 const rootEmoji = String.fromCodePoint(0x1fab4);
 const castEmoji = String.fromCodePoint(0x1f4e2);
 
-const visualizeChains = (chains: SignedCastChain[]): string[] => {
-  return chains.map((chain) => visualizeChain(chain)).flat();
+const visualizeChains = (chains: SignedCastChain[], color: (str: string) => string): string[] => {
+  return chains.map((chain) => visualizeChain(chain, color)).flat();
 };
 
-const visualizeChain = (chain: SignedCastChain): string[] => {
+const visualizeChain = (chain: SignedCastChain, color: (str: string) => string): string[] => {
   return chain.map((msg) => {
     const visual = isCast(msg) ? castEmoji : rootEmoji;
-    return visual + '  ..' + msg.hash.slice(msg.hash.length - 4, msg.hash.length);
+    return color(visual + '  ' + msg.hash.slice(msg.hash.length - 3, msg.hash.length));
   });
+};
+
+/** Determine the timestamp of the latest message in the Cast Chains */
+const latestChainSignedAt = (chains: SignedCastChain[]): number => {
+  const lastChain = chains[chains.length - 1];
+  if (!lastChain) {
+    return 0;
+  }
+
+  const lastMsg = lastChain[lastChain.length - 1];
+  if (!lastMsg) {
+    return 0;
+  }
+
+  return lastMsg.message.signedAt;
 };
 
 let nodes: NodeList;
@@ -27,10 +43,23 @@ const Debugger = {
     const table = new Table();
 
     for (const username of FCNode.usernames) {
+      // Determine the chain which has the user's latest message, so that we can mark it green.
+      let latestSignedAt = 0;
+      nodes.forEach((node) => {
+        const chainLatestSignedAt = latestChainSignedAt(node.engine.getCastChains(username));
+        if (chainLatestSignedAt > latestSignedAt) {
+          latestSignedAt = chainLatestSignedAt;
+        }
+      });
+
+      // For each user's chain in each node, print the chain.
       for (const node of nodes.values()) {
-        table.push({ [node.name]: visualizeChains(node.engine.getCastChains(username)) });
+        const chainLatestSignedAt = latestChainSignedAt(node.engine.getCastChains(username));
+        const color = chainLatestSignedAt === latestSignedAt ? colors.green : colors.red;
+        table.push({ [node.name]: visualizeChains(node.engine.getCastChains(username), color) });
       }
     }
+
     console.log(table.toString());
   },
 };
