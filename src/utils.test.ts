38be362d2f9dc36c6ba6e7a446ab73bcf6e0d70c
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2022-07-30 14:58:18 -0400

    Use lowercased address as signerKey for EthereumSigner and add utils tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,9 @@
-import { hashFCObject, hashCompare } from '~/utils';
+import { ethers } from 'ethers';
+import Faker from 'faker';
+import * as ed from '@noble/ed25519';
+import { hashFCObject, hashCompare, generateEthereumSigner, generateEd25519Signer, convertToHex } from '~/utils';
+import { Ed25519Signer, EthereumSigner } from '~/types';
+import { hexToBytes, utf8ToBytes } from 'ethereum-cryptography/utils';
 
 describe('hashFCObject', () => {
   const blake2bEmptyObject =
@@ -109,5 +114,64 @@ describe('hashCompare', () => {
   test('compare strings first input is uppercase second input is lower case', async () => {
     const cmp = hashCompare('A', 'a');
     expect(cmp).toBeLessThan(0);
+  });
+});
+
+describe('generateEthereumSigner', () => {
+  let signer: EthereumSigner;
+
+  beforeAll(async () => {
+    signer = await generateEthereumSigner();
+  });
+
+  test('signerKey is lowercased address', async () => {
+    const address = await signer.wallet.getAddress();
+    expect(signer.signerKey).toEqual(address.toLowerCase());
+  });
+
+  test('lowercased address is still a valid address', () => {
+    expect(ethers.utils.isAddress(signer.signerKey)).toBe(true);
+  });
+
+  test('text can be signed and verified', async () => {
+    const text = Faker.lorem.sentence(2);
+    const signature = await signer.wallet.signMessage(text);
+    const recoveredAddress = await ethers.utils.verifyMessage(text, signature);
+    expect(recoveredAddress.toLowerCase()).toEqual(signer.signerKey);
+  });
+
+  test('hex can be signed and verified', async () => {
+    const hex = Faker.datatype.hexaDecimal(40);
+    const signature = await signer.wallet.signMessage(hex);
+    const recoveredAddress = await ethers.utils.verifyMessage(hex, signature);
+    expect(recoveredAddress.toLowerCase()).toEqual(signer.signerKey);
+  });
+});
+
+describe('generateEd25519Signer', () => {
+  let signer: Ed25519Signer;
+  let pubKey: string;
+
+  beforeAll(async () => {
+    signer = await generateEd25519Signer();
+    pubKey = await convertToHex(await ed.getPublicKey(signer.privateKey));
+  });
+
+  test('signerKey is public key', () => {
+    expect(signer.signerKey).toEqual(pubKey);
+  });
+
+  test('text can be signed and verified', async () => {
+    const text = Faker.lorem.sentence(2);
+    const signature = await ed.sign(utf8ToBytes(text), signer.privateKey);
+    const isValid = await ed.verify(signature, utf8ToBytes(text), hexToBytes(signer.signerKey));
+    expect(isValid).toBe(true);
+  });
+
+  test('hex can be signed and verified', async () => {
+    const hex = Faker.datatype.hexaDecimal(40);
+    const signature = await ed.sign(hexToBytes(hex), signer.privateKey);
+    const isValid = await ed.verify(signature, hexToBytes(hex), hexToBytes(signer.signerKey));
+    expect(isValid).toBe(true);
   });
 });
