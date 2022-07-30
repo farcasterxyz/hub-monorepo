99eb20f4f13149a221977f1ef4a44166b17d3ed1
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2022-07-30 14:58:18 -0400

    Use lowercased address as signerKey for EthereumSigner and add utils tests

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -361,7 +361,7 @@ class Engine {
     if (message.signatureType === SignatureAlgorithm.EthereumPersonalSign) {
       try {
         const recoveredSigner = ethers.utils.verifyMessage(message.hash, message.signature);
-        if (recoveredSigner !== message.signer) {
+        if (recoveredSigner.toLowerCase() !== message.signer.toLowerCase()) {
           return err('validateMessage: invalid signature');
         }
       } catch (e: any) {
