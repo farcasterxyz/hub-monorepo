e3f5da1a299104dc7fce184b521adf3788059f76
Sagar Dhawan <sdhawan@a16z.com>
2022-09-16 23:46:49 -0700

    Finally fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,21 @@ import type { Config } from '@jest/types';
 
 export default async (): Promise<Config.InitialOptions> => {
   return {
-    preset: 'ts-jest',
+    preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     moduleNameMapper: {
       '^~/(.*)$': '<rootDir>/src/$1',
     },
+    // transform ts files with ts-jest and enable ESM
+    transform: {
+      '^.+\\.tsx?$': [
+        'ts-jest',
+        {
+          tsconfig: 'tsconfig.json',
+          useESM: true,
+        },
+      ],
+    },
+    extensionsToTreatAsEsm: ['.ts'],
   };
 };
