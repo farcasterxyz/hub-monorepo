6e8420e2063ead8d401748334d424e6009c58ed6
Varun Srinivasan <varunsrin@gmail.com>
2022-10-03 16:26:52 -0700

    test: fix yarn jest failing on node.test.ts (#142)
    * test: fix yarn jest failing on node.test.ts
    
    * ci: remove --runInBand which does not seem necessary

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,23 @@
-import type { Config } from '@jest/types';
+import type { JestConfigWithTsJest } from 'ts-jest';
 
-export default async (): Promise<Config.InitialOptions> => {
-  return {
-    preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-      '^~/(.*)$': '<rootDir>/src/$1',
-    },
-    coveragePathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
-    testPathIgnorePatterns: ['<rootDir>/build', '<rootDir>/node_modules'],
-    // transform ts files with ts-jest and enable ESM
-    transform: {
-      '^.+\\.tsx?$': [
-        'ts-jest',
-        {
-          tsconfig: 'tsconfig.json',
-          useESM: true,
-        },
-      ],
-    },
-    extensionsToTreatAsEsm: ['.ts'],
-  };
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/src/$1',
+  },
+  coveragePathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ['<rootDir>/build', '<rootDir>/node_modules'],
+  // transform ts files with ts-jest and enable ESM
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        useESM: true,
+      },
+    ],
+  },
 };
+
+export default jestConfig;
