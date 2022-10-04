7f2fd0e7dd5b4883b129259a2fd984b8f417659e
Sagar <107961892+sagar-a16z@users.noreply.github.com>
2022-10-04 16:44:53 -0700

    feat: hub executable (#140)
    * Feat: Hub executable
    
    * fix: improve hub test for "reset-db" and address review comments

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,50 @@
-import { hashCompare } from '~/utils';
+import { Command } from 'commander';
+import { Hub, HubOpts } from '~/hub';
 
-// Import something external to make sure it works!
-console.log(hashCompare('Hello, world!', '1'));
+const app = new Command();
+
+app
+  .name('farcaster-hub')
+  .description('Farcaster Hub')
+  .version(process.env.npm_package_version ?? '1.0.0');
+
+app
+  .option('-N, --network-url <url>', 'ID Registry network URL')
+  .option('-A, --id-registry-address <address>', 'ID Registry address')
+  .option('-B, --bootstrap-addresses <addresses...>', 'A list of MultiAddrs to use for bootstrapping')
+  .option('--rpc-port <port>', 'The RPC port to use. (default: selects one at random')
+  .option('--simple-sync <enabled>', 'Enable/Disable simple sync', true)
+  .option('--db-reset', 'Clear the database before starting', false)
+  .option('--db-name <name>', 'The name of the RocksDB instance', 'rocks.hub._default');
+
+const teardown = async (hub: Hub) => {
+  await hub.stop();
+  process.exit();
+};
+
+app.parse(process.argv);
+const cliOptions = app.opts();
+console.log(cliOptions);
+const options: HubOpts = {
+  networkUrl: cliOptions.networkUrl,
+  IDRegistryAddress: cliOptions.idRegistryAddress,
+  bootstrapAddrs: cliOptions.bootstrapAddresses,
+  port: cliOptions.rpcPort,
+  simpleSync: cliOptions.simpleSync,
+  rocksDBName: cliOptions.dbName,
+  resetDB: cliOptions.dbReset,
+};
+
+const hub = new Hub(options);
+hub.start();
+process.stdin.resume();
+
+process.on('SIGINT', async () => {
+  await teardown(hub);
+});
+process.on('SIGTERM', async () => {
+  await teardown(hub);
+});
+process.on('SIGQUIT', async () => {
+  await teardown(hub);
+});
