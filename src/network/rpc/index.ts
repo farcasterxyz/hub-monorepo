8acc1dbbc0baeac83a077475a6938b66291efec5
Varun Srinivasan <varunsrin@gmail.com>
2022-10-10 15:04:01 -0700

    fix(rpc): use export * instead of named import + export because of node --experimental-modules bug

--- src/network/rpc/index.ts
+++ src/network/rpc/index.ts
@@ -2,6 +2,10 @@
 
 import { RPCClient } from './client';
 import { RPCServer } from './server';
-import { RPCHandler } from './interfaces';
 
-export { RPCClient, RPCServer, RPCHandler };
+// node --experimental-modules does not play nicely with exports and refuses to import a default interface even when
+// implementing the fix described here: https://github.com/ardatan/graphql-tools/issues/913
+// Requires further investigating, but export * seems like a decent workaround for now
+export * from './interfaces';
+
+export { RPCClient, RPCServer };
