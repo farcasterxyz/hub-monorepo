69bf1738e904c612cb3e092221e2589b5f92419c
Varun Srinivasan <varunsrin@gmail.com>
2022-10-11 10:41:41 -0700

    chore: add eslint-plugin-security to linting to catch security issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:security/recommended',
   ],
   plugins: ['prefer-arrow-functions'],
   rules: {
