118d121803dfa9bb1f901db1a54b8826717ebb65
Tim Daubenschütz <tim@daubenschuetz.de>
2022-12-07 17:59:52 +0100

    Fix and suppress eslint warnings
    - Fixes #163

--- apps/hub/src/network/sync/merkleTrie.ts
+++ apps/hub/src/network/sync/merkleTrie.ts
@@ -64,6 +64,8 @@ class MerkleTrie {
   public getDivergencePrefix(prefix: string, excludedHashes: string[]): string {
     const ourExcludedHashes = this.getSnapshot(prefix).excludedHashes;
     for (let i = 0; i < prefix.length; i++) {
+      // NOTE: `i` is controlled by for loop and hence not at risk of object injection.
+      // eslint-disable-next-line security/detect-object-injection
       if (ourExcludedHashes[i] !== excludedHashes[i]) {
         return prefix.slice(0, i);
       }
