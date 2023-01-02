ce23c7cca789a5b2678e3bb88e6cb5430622cd85
pavan-nambi <pavannambi999@gmail.com>
2023-01-02 10:50:15 +0530

    update(348)

---
+++ packages/prettier-config-custom/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  useTabs: true,
+  singleQuote: true,
+  quoteProps: "preserve",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
