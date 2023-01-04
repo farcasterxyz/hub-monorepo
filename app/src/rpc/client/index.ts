bd354a5da36694b32badd24e56df6ea33fa0f62b
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2023-01-04 18:49:39 -0500

    refactor sync client methods to use makeUnaryRequest

--- app/src/rpc/client/index.ts
+++ app/src/rpc/client/index.ts
@@ -1,35 +1,13 @@
-import grpc from '@grpc/grpc-js';
 import { bytesToUtf8String, utf8StringToBytes } from '@hub/bytes';
-import { HubAsyncResult, HubError, HubErrorCode } from '@hub/errors';
+import { HubAsyncResult } from '@hub/errors';
 import * as flatbuffers from '@hub/flatbuffers';
 import { HubInfoResponse } from '@hub/flatbuffers';
 import Client from '@hub/grpc-client';
-import { ByteBuffer } from 'flatbuffers';
-import { err, ok } from 'neverthrow';
 import MessageModel from '~/flatbuffers/models/messageModel';
 import { NodeMetadata } from '~/network/sync/merkleTrie';
 import { TrieSnapshot } from '~/network/sync/trieNode';
 import syncRequests from '~/rpc/client/syncRequests';
 import { syncDefinition } from '~/rpc/syncDefinitions';
-
-const fromServiceError = (err: grpc.ServiceError): HubError => {
-  return new HubError(err.metadata.get('errCode')[0] as HubErrorCode, err.details);
-};
-
-interface GenericFlatbuffer {
-  bb: ByteBuffer | null;
-}
-
-export const defaultMethod = {
-  requestStream: false,
-  responseStream: false,
-  requestSerialize: (request: GenericFlatbuffer): Buffer => {
-    return Buffer.from(request.bb?.bytes() ?? new Uint8Array());
-  },
-  responseSerialize: (response: GenericFlatbuffer): Buffer => {
-    return Buffer.from(response.bb?.bytes() ?? new Uint8Array());
-  },
-};
 
 const fromNodeMetadataResponse = (response: flatbuffers.TrieNodeMetadataResponse): NodeMetadata => {
   const children = new Map<string, NodeMetadata>();
@@ -81,131 +59,40 @@ const fromSyncIdsByPrefixResponse = (response: flatbuffers.GetAllSyncIdsByPrefix
 };
 
 class HubClient extends Client {
-  /* -------------------------------------------------------------------------- */
-  /*                                   Sync Methods                             */
-  /* -------------------------------------------------------------------------- */
   async getInfo(): HubAsyncResult<HubInfoResponse> {
-    return this.makeUnaryInfoRequest(syncDefinition().getInfo, syncRequests.getInfoRequest());
+    return this.makeUnaryRequest(syncDefinition().getInfo, syncRequests.getInfoRequest());
   }
 
   async getSyncMetadataByPrefix(prefix: string): HubAsyncResult<NodeMetadata> {
-    return this.makeUnarySyncNodeMetadataRequest(
+    const response = await this.makeUnaryRequest(
       syncDefinition().getSyncMetadataByPrefix,
       syncRequests.createByPrefixRequest(utf8StringToBytes(prefix)._unsafeUnwrap())
     );
+    return response.map((metadataResponse) => fromNodeMetadataResponse(metadataResponse));
   }
 
   async getSyncTrieNodeSnapshotByPrefix(prefix: string): HubAsyncResult<{ snapshot: TrieSnapshot; rootHash: string }> {
-    return this.makeUnarySyncNodeSnapshotRequest(
+    const response = await this.makeUnaryRequest(
       syncDefinition().getSyncTrieNodeSnapshotByPrefix,
       syncRequests.createByPrefixRequest(utf8StringToBytes(prefix)._unsafeUnwrap())
     );
+    return response.map((snapshotResponse) => fromNodeSnapshotResponse(snapshotResponse));
   }
 
   async getAllMessagesBySyncIds(hashes: Uint8Array[]): HubAsyncResult<MessageModel[]> {
-    const messages = await this.makeUnaryMessagesRequest(
+    const response = await this.makeUnaryMessagesRequest(
       syncDefinition().getAllMessagesBySyncIds,
       syncRequests.getAllMessagesByHashesRequest(hashes)
     );
-    if (messages.isErr()) {
-      return err(messages.error);
-    }
-    return ok(messages.value.map((message) => new MessageModel(message)));
+    return response.map((messages) => messages.map((message) => new MessageModel(message)));
   }
 
   async getSyncIdsByPrefix(prefix: string): HubAsyncResult<string[]> {
-    return this.makeUnarySyncIdsByPrefixRequest(
+    const response = await this.makeUnaryRequest(
       syncDefinition().getAllSyncIdsByPrefix,
       syncRequests.createByPrefixRequest(utf8StringToBytes(prefix)._unsafeUnwrap())
     );
-  }
-
-  /* -------------------------------------------------------------------------- */
-  /*                               Private Methods                              */
-  /* -------------------------------------------------------------------------- */
-
-  private makeUnaryInfoRequest<RequestType>(
-    method: grpc.MethodDefinition<RequestType, flatbuffers.HubInfoResponse>,
-    request: RequestType
-  ): HubAsyncResult<HubInfoResponse> {
-    return new Promise((resolve) => {
-      this.client.makeUnaryRequest(
-        method.path,
-        method.requestSerialize,
-        method.responseDeserialize,
-        request,
-        (e: grpc.ServiceError | null, response?: flatbuffers.HubInfoResponse) => {
-          if (e) {
-            resolve(err(fromServiceError(e)));
-          } else if (response) {
-            resolve(ok(response));
-          }
-        }
-      );
-    });
-  }
-
-  private makeUnarySyncNodeMetadataRequest<RequestType>(
-    method: grpc.MethodDefinition<RequestType, flatbuffers.TrieNodeMetadataResponse>,
-    request: RequestType
-  ): HubAsyncResult<NodeMetadata> {
-    return new Promise((resolve) => {
-      this.client.makeUnaryRequest(
-        method.path,
-        method.requestSerialize,
-        method.responseDeserialize,
-        request,
-        (e: grpc.ServiceError | null, response?: flatbuffers.TrieNodeMetadataResponse) => {
-          if (e) {
-            resolve(err(fromServiceError(e)));
-          } else if (response) {
-            resolve(ok(fromNodeMetadataResponse(response)));
-          }
-        }
-      );
-    });
-  }
-
-  private makeUnarySyncNodeSnapshotRequest<RequestType>(
-    method: grpc.MethodDefinition<RequestType, flatbuffers.TrieNodeSnapshotResponse>,
-    request: RequestType
-  ): HubAsyncResult<{ snapshot: TrieSnapshot; rootHash: string }> {
-    return new Promise((resolve) => {
-      this.client.makeUnaryRequest(
-        method.path,
-        method.requestSerialize,
-        method.responseDeserialize,
-        request,
-        (e: grpc.ServiceError | null, response?: flatbuffers.TrieNodeSnapshotResponse) => {
-          if (e) {
-            resolve(err(fromServiceError(e)));
-          } else if (response) {
-            resolve(ok(fromNodeSnapshotResponse(response)));
-          }
-        }
-      );
-    });
-  }
-
-  private makeUnarySyncIdsByPrefixRequest<RequestType>(
-    method: grpc.MethodDefinition<RequestType, flatbuffers.GetAllSyncIdsByPrefixResponse>,
-    request: RequestType
-  ): HubAsyncResult<string[]> {
-    return new Promise((resolve) => {
-      this.client.makeUnaryRequest(
-        method.path,
-        method.requestSerialize,
-        method.responseDeserialize,
-        request,
-        (e: grpc.ServiceError | null, response?: flatbuffers.GetAllSyncIdsByPrefixResponse) => {
-          if (e) {
-            resolve(err(fromServiceError(e)));
-          } else if (response) {
-            resolve(ok(fromSyncIdsByPrefixResponse(response)));
-          }
-        }
-      );
-    });
+    return response.map((prefixResponse) => fromSyncIdsByPrefixResponse(prefixResponse));
   }
 }
 
