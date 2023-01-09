d5e2c894214c177d518bb7c5edca8eff14e97215
Paul Fletcher-Hill <1607180+pfletcherhill@users.noreply.github.com>
2023-01-09 15:03:51 -0500

    chore: add readme to @farcaster/js (#396)

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -1,4 +1,5 @@
+export { Ed25519Signer, Eip712Signer } from '@hub/utils';
 export * from './builders';
 export * from './client';
-export * from './types';
-export * from './utils';
+export * as types from './types';
+export * as utils from './utils';
