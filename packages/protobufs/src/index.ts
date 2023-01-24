f700342b88b945d1ca7064afcd1bac843dd14e79
Aditya Kulkarni <adityapk@gmail.com>
2023-01-24 15:53:15 -0600

    feat: Add RPC implementation

--- packages/protobufs/src/index.ts
+++ packages/protobufs/src/index.ts
@@ -1,3 +1,15 @@
+import * as grpc from '@grpc/grpc-js';
+
+export * from './generated/gossip';
+export * from './generated/hub_state';
+export * from './generated/id_registry_event';
 export * from './generated/message';
+export * from './generated/rpc';
 export * from './typeguards';
 export * from './types';
+
+export const getServer = (): grpc.Server => {
+  const server = new grpc.Server();
+
+  return server;
+};
