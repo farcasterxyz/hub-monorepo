48ff2505704a216c8dd45d10c9803689bd3a10f5
Aditya Kulkarni <adityapk@gmail.com>
2023-01-24 11:47:20 -0600

    feat: Sync + Gossip protos and RPC service

---
+++ packages/protobufs/src/generated/rpc.ts
@@ -0,0 +1,573 @@
+/* eslint-disable */
+import Long from "long";
+import _m0 from "protobufjs/minimal";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Message } from "./message";
+
+export interface Empty {
+}
+
+/** Response Types for the Sync RPC Methods */
+export interface HubInfoResponse {
+  version: string;
+  isSynced: boolean;
+  nickname: string;
+  rootHash: Uint8Array;
+}
+
+export interface TrieNodeMetadataResponse {
+  prefix: Uint8Array;
+  numMessages: number;
+  hash: Uint8Array;
+  children: TrieNodeMetadataResponse[];
+}
+
+export interface TrieNodeSnapshotResponse {
+  prefix: Uint8Array;
+  excludedHashes: string[];
+  numMessages: number;
+  rootHash: Uint8Array;
+}
+
+export interface TrieNodePrefix {
+  prefix: Uint8Array;
+}
+
+export interface SyncIds {
+  syncIds: string[];
+}
+
+function createBaseEmpty(): Empty {
+  return {};
+}
+
+export const Empty = {
+  encode(_: Empty, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): Empty {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseEmpty();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(_: any): Empty {
+    return {};
+  },
+
+  toJSON(_: Empty): unknown {
+    const obj: any = {};
+    return obj;
+  },
+
+  create<I extends Exact<DeepPartial<Empty>, I>>(base?: I): Empty {
+    return Empty.fromPartial(base ?? {});
+  },
+
+  fromPartial<I extends Exact<DeepPartial<Empty>, I>>(_: I): Empty {
+    const message = createBaseEmpty();
+    return message;
+  },
+};
+
+function createBaseHubInfoResponse(): HubInfoResponse {
+  return { version: "", isSynced: false, nickname: "", rootHash: new Uint8Array() };
+}
+
+export const HubInfoResponse = {
+  encode(message: HubInfoResponse, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.version !== "") {
+      writer.uint32(10).string(message.version);
+    }
+    if (message.isSynced === true) {
+      writer.uint32(16).bool(message.isSynced);
+    }
+    if (message.nickname !== "") {
+      writer.uint32(26).string(message.nickname);
+    }
+    if (message.rootHash.length !== 0) {
+      writer.uint32(34).bytes(message.rootHash);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): HubInfoResponse {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseHubInfoResponse();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.version = reader.string();
+          break;
+        case 2:
+          message.isSynced = reader.bool();
+          break;
+        case 3:
+          message.nickname = reader.string();
+          break;
+        case 4:
+          message.rootHash = reader.bytes();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): HubInfoResponse {
+    return {
+      version: isSet(object.version) ? String(object.version) : "",
+      isSynced: isSet(object.isSynced) ? Boolean(object.isSynced) : false,
+      nickname: isSet(object.nickname) ? String(object.nickname) : "",
+      rootHash: isSet(object.rootHash) ? bytesFromBase64(object.rootHash) : new Uint8Array(),
+    };
+  },
+
+  toJSON(message: HubInfoResponse): unknown {
+    const obj: any = {};
+    message.version !== undefined && (obj.version = message.version);
+    message.isSynced !== undefined && (obj.isSynced = message.isSynced);
+    message.nickname !== undefined && (obj.nickname = message.nickname);
+    message.rootHash !== undefined &&
+      (obj.rootHash = base64FromBytes(message.rootHash !== undefined ? message.rootHash : new Uint8Array()));
+    return obj;
+  },
+
+  create<I extends Exact<DeepPartial<HubInfoResponse>, I>>(base?: I): HubInfoResponse {
+    return HubInfoResponse.fromPartial(base ?? {});
+  },
+
+  fromPartial<I extends Exact<DeepPartial<HubInfoResponse>, I>>(object: I): HubInfoResponse {
+    const message = createBaseHubInfoResponse();
+    message.version = object.version ?? "";
+    message.isSynced = object.isSynced ?? false;
+    message.nickname = object.nickname ?? "";
+    message.rootHash = object.rootHash ?? new Uint8Array();
+    return message;
+  },
+};
+
+function createBaseTrieNodeMetadataResponse(): TrieNodeMetadataResponse {
+  return { prefix: new Uint8Array(), numMessages: 0, hash: new Uint8Array(), children: [] };
+}
+
+export const TrieNodeMetadataResponse = {
+  encode(message: TrieNodeMetadataResponse, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.prefix.length !== 0) {
+      writer.uint32(10).bytes(message.prefix);
+    }
+    if (message.numMessages !== 0) {
+      writer.uint32(16).uint64(message.numMessages);
+    }
+    if (message.hash.length !== 0) {
+      writer.uint32(26).bytes(message.hash);
+    }
+    for (const v of message.children) {
+      TrieNodeMetadataResponse.encode(v!, writer.uint32(34).fork()).ldelim();
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): TrieNodeMetadataResponse {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseTrieNodeMetadataResponse();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.prefix = reader.bytes();
+          break;
+        case 2:
+          message.numMessages = longToNumber(reader.uint64() as Long);
+          break;
+        case 3:
+          message.hash = reader.bytes();
+          break;
+        case 4:
+          message.children.push(TrieNodeMetadataResponse.decode(reader, reader.uint32()));
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): TrieNodeMetadataResponse {
+    return {
+      prefix: isSet(object.prefix) ? bytesFromBase64(object.prefix) : new Uint8Array(),
+      numMessages: isSet(object.numMessages) ? Number(object.numMessages) : 0,
+      hash: isSet(object.hash) ? bytesFromBase64(object.hash) : new Uint8Array(),
+      children: Array.isArray(object?.children)
+        ? object.children.map((e: any) => TrieNodeMetadataResponse.fromJSON(e))
+        : [],
+    };
+  },
+
+  toJSON(message: TrieNodeMetadataResponse): unknown {
+    const obj: any = {};
+    message.prefix !== undefined &&
+      (obj.prefix = base64FromBytes(message.prefix !== undefined ? message.prefix : new Uint8Array()));
+    message.numMessages !== undefined && (obj.numMessages = Math.round(message.numMessages));
+    message.hash !== undefined &&
+      (obj.hash = base64FromBytes(message.hash !== undefined ? message.hash : new Uint8Array()));
+    if (message.children) {
+      obj.children = message.children.map((e) => e ? TrieNodeMetadataResponse.toJSON(e) : undefined);
+    } else {
+      obj.children = [];
+    }
+    return obj;
+  },
+
+  create<I extends Exact<DeepPartial<TrieNodeMetadataResponse>, I>>(base?: I): TrieNodeMetadataResponse {
+    return TrieNodeMetadataResponse.fromPartial(base ?? {});
+  },
+
+  fromPartial<I extends Exact<DeepPartial<TrieNodeMetadataResponse>, I>>(object: I): TrieNodeMetadataResponse {
+    const message = createBaseTrieNodeMetadataResponse();
+    message.prefix = object.prefix ?? new Uint8Array();
+    message.numMessages = object.numMessages ?? 0;
+    message.hash = object.hash ?? new Uint8Array();
+    message.children = object.children?.map((e) => TrieNodeMetadataResponse.fromPartial(e)) || [];
+    return message;
+  },
+};
+
+function createBaseTrieNodeSnapshotResponse(): TrieNodeSnapshotResponse {
+  return { prefix: new Uint8Array(), excludedHashes: [], numMessages: 0, rootHash: new Uint8Array() };
+}
+
+export const TrieNodeSnapshotResponse = {
+  encode(message: TrieNodeSnapshotResponse, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.prefix.length !== 0) {
+      writer.uint32(10).bytes(message.prefix);
+    }
+    for (const v of message.excludedHashes) {
+      writer.uint32(18).string(v!);
+    }
+    if (message.numMessages !== 0) {
+      writer.uint32(24).uint64(message.numMessages);
+    }
+    if (message.rootHash.length !== 0) {
+      writer.uint32(34).bytes(message.rootHash);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): TrieNodeSnapshotResponse {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseTrieNodeSnapshotResponse();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.prefix = reader.bytes();
+          break;
+        case 2:
+          message.excludedHashes.push(reader.string());
+          break;
+        case 3:
+          message.numMessages = longToNumber(reader.uint64() as Long);
+          break;
+        case 4:
+          message.rootHash = reader.bytes();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): TrieNodeSnapshotResponse {
+    return {
+      prefix: isSet(object.prefix) ? bytesFromBase64(object.prefix) : new Uint8Array(),
+      excludedHashes: Array.isArray(object?.excludedHashes) ? object.excludedHashes.map((e: any) => String(e)) : [],
+      numMessages: isSet(object.numMessages) ? Number(object.numMessages) : 0,
+      rootHash: isSet(object.rootHash) ? bytesFromBase64(object.rootHash) : new Uint8Array(),
+    };
+  },
+
+  toJSON(message: TrieNodeSnapshotResponse): unknown {
+    const obj: any = {};
+    message.prefix !== undefined &&
+      (obj.prefix = base64FromBytes(message.prefix !== undefined ? message.prefix : new Uint8Array()));
+    if (message.excludedHashes) {
+      obj.excludedHashes = message.excludedHashes.map((e) => e);
+    } else {
+      obj.excludedHashes = [];
+    }
+    message.numMessages !== undefined && (obj.numMessages = Math.round(message.numMessages));
+    message.rootHash !== undefined &&
+      (obj.rootHash = base64FromBytes(message.rootHash !== undefined ? message.rootHash : new Uint8Array()));
+    return obj;
+  },
+
+  create<I extends Exact<DeepPartial<TrieNodeSnapshotResponse>, I>>(base?: I): TrieNodeSnapshotResponse {
+    return TrieNodeSnapshotResponse.fromPartial(base ?? {});
+  },
+
+  fromPartial<I extends Exact<DeepPartial<TrieNodeSnapshotResponse>, I>>(object: I): TrieNodeSnapshotResponse {
+    const message = createBaseTrieNodeSnapshotResponse();
+    message.prefix = object.prefix ?? new Uint8Array();
+    message.excludedHashes = object.excludedHashes?.map((e) => e) || [];
+    message.numMessages = object.numMessages ?? 0;
+    message.rootHash = object.rootHash ?? new Uint8Array();
+    return message;
+  },
+};
+
+function createBaseTrieNodePrefix(): TrieNodePrefix {
+  return { prefix: new Uint8Array() };
+}
+
+export const TrieNodePrefix = {
+  encode(message: TrieNodePrefix, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.prefix.length !== 0) {
+      writer.uint32(10).bytes(message.prefix);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): TrieNodePrefix {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseTrieNodePrefix();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.prefix = reader.bytes();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): TrieNodePrefix {
+    return { prefix: isSet(object.prefix) ? bytesFromBase64(object.prefix) : new Uint8Array() };
+  },
+
+  toJSON(message: TrieNodePrefix): unknown {
+    const obj: any = {};
+    message.prefix !== undefined &&
+      (obj.prefix = base64FromBytes(message.prefix !== undefined ? message.prefix : new Uint8Array()));
+    return obj;
+  },
+
+  create<I extends Exact<DeepPartial<TrieNodePrefix>, I>>(base?: I): TrieNodePrefix {
+    return TrieNodePrefix.fromPartial(base ?? {});
+  },
+
+  fromPartial<I extends Exact<DeepPartial<TrieNodePrefix>, I>>(object: I): TrieNodePrefix {
+    const message = createBaseTrieNodePrefix();
+    message.prefix = object.prefix ?? new Uint8Array();
+    return message;
+  },
+};
+
+function createBaseSyncIds(): SyncIds {
+  return { syncIds: [] };
+}
+
+export const SyncIds = {
+  encode(message: SyncIds, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    for (const v of message.syncIds) {
+      writer.uint32(10).string(v!);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): SyncIds {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseSyncIds();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.syncIds.push(reader.string());
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): SyncIds {
+    return { syncIds: Array.isArray(object?.syncIds) ? object.syncIds.map((e: any) => String(e)) : [] };
+  },
+
+  toJSON(message: SyncIds): unknown {
+    const obj: any = {};
+    if (message.syncIds) {
+      obj.syncIds = message.syncIds.map((e) => e);
+    } else {
+      obj.syncIds = [];
+    }
+    return obj;
+  },
+
+  create<I extends Exact<DeepPartial<SyncIds>, I>>(base?: I): SyncIds {
+    return SyncIds.fromPartial(base ?? {});
+  },
+
+  fromPartial<I extends Exact<DeepPartial<SyncIds>, I>>(object: I): SyncIds {
+    const message = createBaseSyncIds();
+    message.syncIds = object.syncIds?.map((e) => e) || [];
+    return message;
+  },
+};
+
+export interface SyncService {
+  GetInfo(request: Empty): Promise<HubInfoResponse>;
+  GetAllSyncIdsByPrefix(request: TrieNodePrefix): Promise<SyncIds>;
+  GetAllMessagesBySyncIds(request: SyncIds): Observable<Message>;
+  GetSyncMetadataByPrefix(request: TrieNodePrefix): Promise<TrieNodeMetadataResponse>;
+  GetSyncSnapshotByPrefix(request: TrieNodePrefix): Promise<TrieNodeSnapshotResponse>;
+}
+
+export class SyncServiceClientImpl implements SyncService {
+  private readonly rpc: Rpc;
+  private readonly service: string;
+  constructor(rpc: Rpc, opts?: { service?: string }) {
+    this.service = opts?.service || "SyncService";
+    this.rpc = rpc;
+    this.GetInfo = this.GetInfo.bind(this);
+    this.GetAllSyncIdsByPrefix = this.GetAllSyncIdsByPrefix.bind(this);
+    this.GetAllMessagesBySyncIds = this.GetAllMessagesBySyncIds.bind(this);
+    this.GetSyncMetadataByPrefix = this.GetSyncMetadataByPrefix.bind(this);
+    this.GetSyncSnapshotByPrefix = this.GetSyncSnapshotByPrefix.bind(this);
+  }
+  GetInfo(request: Empty): Promise<HubInfoResponse> {
+    const data = Empty.encode(request).finish();
+    const promise = this.rpc.request(this.service, "GetInfo", data);
+    return promise.then((data) => HubInfoResponse.decode(new _m0.Reader(data)));
+  }
+
+  GetAllSyncIdsByPrefix(request: TrieNodePrefix): Promise<SyncIds> {
+    const data = TrieNodePrefix.encode(request).finish();
+    const promise = this.rpc.request(this.service, "GetAllSyncIdsByPrefix", data);
+    return promise.then((data) => SyncIds.decode(new _m0.Reader(data)));
+  }
+
+  GetAllMessagesBySyncIds(request: SyncIds): Observable<Message> {
+    const data = SyncIds.encode(request).finish();
+    const result = this.rpc.serverStreamingRequest(this.service, "GetAllMessagesBySyncIds", data);
+    return result.pipe(map((data) => Message.decode(new _m0.Reader(data))));
+  }
+
+  GetSyncMetadataByPrefix(request: TrieNodePrefix): Promise<TrieNodeMetadataResponse> {
+    const data = TrieNodePrefix.encode(request).finish();
+    const promise = this.rpc.request(this.service, "GetSyncMetadataByPrefix", data);
+    return promise.then((data) => TrieNodeMetadataResponse.decode(new _m0.Reader(data)));
+  }
+
+  GetSyncSnapshotByPrefix(request: TrieNodePrefix): Promise<TrieNodeSnapshotResponse> {
+    const data = TrieNodePrefix.encode(request).finish();
+    const promise = this.rpc.request(this.service, "GetSyncSnapshotByPrefix", data);
+    return promise.then((data) => TrieNodeSnapshotResponse.decode(new _m0.Reader(data)));
+  }
+}
+
+interface Rpc {
+  request(service: string, method: string, data: Uint8Array): Promise<Uint8Array>;
+  clientStreamingRequest(service: string, method: string, data: Observable<Uint8Array>): Promise<Uint8Array>;
+  serverStreamingRequest(service: string, method: string, data: Uint8Array): Observable<Uint8Array>;
+  bidirectionalStreamingRequest(service: string, method: string, data: Observable<Uint8Array>): Observable<Uint8Array>;
+}
+
+declare var self: any | undefined;
+declare var window: any | undefined;
+declare var global: any | undefined;
+var tsProtoGlobalThis: any = (() => {
+  if (typeof globalThis !== "undefined") {
+    return globalThis;
+  }
+  if (typeof self !== "undefined") {
+    return self;
+  }
+  if (typeof window !== "undefined") {
+    return window;
+  }
+  if (typeof global !== "undefined") {
+    return global;
+  }
+  throw "Unable to locate global object";
+})();
+
+function bytesFromBase64(b64: string): Uint8Array {
+  if (tsProtoGlobalThis.Buffer) {
+    return Uint8Array.from(tsProtoGlobalThis.Buffer.from(b64, "base64"));
+  } else {
+    const bin = tsProtoGlobalThis.atob(b64);
+    const arr = new Uint8Array(bin.length);
+    for (let i = 0; i < bin.length; ++i) {
+      arr[i] = bin.charCodeAt(i);
+    }
+    return arr;
+  }
+}
+
+function base64FromBytes(arr: Uint8Array): string {
+  if (tsProtoGlobalThis.Buffer) {
+    return tsProtoGlobalThis.Buffer.from(arr).toString("base64");
+  } else {
+    const bin: string[] = [];
+    arr.forEach((byte) => {
+      bin.push(String.fromCharCode(byte));
+    });
+    return tsProtoGlobalThis.btoa(bin.join(""));
+  }
+}
+
+type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
+
+type DeepPartial<T> = T extends Builtin ? T
+  : T extends Array<infer U> ? Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
+  : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
+
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+type Exact<P, I extends P> = P extends Builtin ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
+
+function longToNumber(long: Long): number {
+  if (long.gt(Number.MAX_SAFE_INTEGER)) {
+    throw new tsProtoGlobalThis.Error("Value is larger than Number.MAX_SAFE_INTEGER");
+  }
+  return long.toNumber();
+}
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any;
+  _m0.configure();
+}
+
+function isSet(value: any): boolean {
+  return value !== null && value !== undefined;
+}
