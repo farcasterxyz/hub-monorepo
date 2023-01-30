25fbb088ae843de6b270ce5978b8dca1ab9a070e
Aditya Kulkarni <adityapk@gmail.com>
2023-01-29 20:26:55 -0600

    feat: Move debug console and cli.ts

---
+++ apps/protohub/src/console/infoCommand.ts
@@ -0,0 +1,30 @@
+import { Empty } from '@farcaster/protobufs';
+import { HubError } from '@farcaster/protoutils';
+import { HubRpcClient } from '~/rpc/client';
+import { ConsoleCommandInterface } from './console';
+
+export class InfoCommand implements ConsoleCommandInterface {
+  constructor(private readonly rpcClient: HubRpcClient) {}
+  commandName(): string {
+    return 'info';
+  }
+  shortHelp(): string {
+    return 'Get the hub version info';
+  }
+  help(): string {
+    return `Usage: info
+    `;
+  }
+
+  info = async () => {
+    const result = await this.rpcClient.getInfo(Empty.create({}));
+    return result.match<any>(
+      (info) => {
+        return info;
+      },
+      (err: HubError) => {
+        return err;
+      }
+    );
+  };
+}
