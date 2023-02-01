89ce3b6d3bab6ce90a47d36f883cb0cef271da94
Aditya Kulkarni <adityapk@gmail.com>
2023-01-31 20:16:21 -0600

    fix: Improve concurrency test

--- apps/hubble/src/storage/engine/seed.ts
+++ apps/hubble/src/storage/engine/seed.ts
@@ -1,17 +1,25 @@
-import { Factories } from '@farcaster/utils';
+import { Eip712Signer, Factories } from '@farcaster/utils';
 import Engine from '~/storage/engine';
 
 /** Util to seed engine with all the data needed to make a signer valid for an fid */
-export const seedSigner = async (engine: Engine, fid: number, signer: Uint8Array) => {
-  const ethSigner = Factories.Eip712Signer.build();
+export const seedSigner = async (
+  engine: Engine,
+  fid: number,
+  signer: Uint8Array,
+  ethSigner?: Eip712Signer
+): Promise<Eip712Signer> => {
+  if (!ethSigner) {
+    ethSigner = Factories.Eip712Signer.build();
 
-  /** Generate and merge ID Registry event linking the fid to the eth wallet */
-  const idRegistryEvent = Factories.IdRegistryEvent.build({
-    fid,
-    to: ethSigner.signerKey,
-  });
+    /** Generate and merge ID Registry event linking the fid to the eth wallet */
+    const idRegistryEvent = Factories.IdRegistryEvent.build({
+      fid,
+      to: ethSigner.signerKey,
+    });
 
-  await engine.mergeIdRegistryEvent(idRegistryEvent);
+    const r = await engine.mergeIdRegistryEvent(idRegistryEvent);
+    expect(r.isOk()).toBeTruthy();
+  }
 
   /** Generate and merge SignerAdd linking the signer to the fid and signed by the eth wallet */
   const signerAdd = await Factories.SignerAddMessage.create(
@@ -21,5 +29,8 @@ export const seedSigner = async (engine: Engine, fid: number, signer: Uint8Array
     { transient: { signer: ethSigner } }
   );
 
-  await engine.mergeMessage(signerAdd);
+  const r = await engine.mergeMessage(signerAdd);
+  expect(r.isOk()).toBeTruthy();
+
+  return ethSigner;
 };
