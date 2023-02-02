b78df238cdea3756c47b00bc56b7d9f5c4495936
Chu Ka-cheong <kc@niomonid.com>
2023-02-03 00:13:58 +0800

    feat: add benchmark suite for MerkleTrie (#470)

---
+++ apps/hubble/src/test/bench/utils.ts
@@ -0,0 +1,38 @@
+/* eslint-disable security/detect-non-literal-fs-filename */
+import * as fs from 'node:fs';
+
+import { stringify } from 'csv-stringify';
+import { Writable } from 'node:stream';
+
+export const outputWriter = (f: string | fs.WriteStream): Writable => {
+  let stream: fs.WriteStream;
+  if (typeof f === 'string') {
+    stream = fs.createWriteStream(f);
+  } else {
+    stream = f as fs.WriteStream;
+  }
+
+  const stringifier = stringify({});
+  stringifier.pipe(stream);
+
+  return stringifier;
+};
+
+export const yieldToEventLoop = (): Promise<void> => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, 0);
+  });
+};
+
+export const waitForPromise = (promise: Promise<unknown>) => {
+  let finished = false;
+  promise.finally(() => (finished = true));
+
+  const pollToFinish = () => {
+    if (!finished) {
+      setTimeout(pollToFinish, 1000);
+    }
+  };
+
+  pollToFinish();
+};
