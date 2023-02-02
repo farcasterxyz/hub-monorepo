746491634228bd5c9c3e3716caff4e262ea333eb
Chu Ka-cheong <kc@niomonid.com>
2023-02-03 00:19:15 +0800

    fix: memory leak in MerkleTrie (#502)
    Node's Buffer uses pooled memory chunk of 8KB by default. The memory
    pool won't be freed until all Buffers that reference the pool is freed.
    TrieNode creates a bunch of temporary Buffers during MerkleTrie.insert.
    And it has a long-lived Buffer that stores the key of an entry.
    
    As a result, the short-lived Buffers creates a lot of holes in memory
    pools, but pools can’t be freed because part of them are referenced by
    long-lived TrieNodes.
    
    The fix is to create new Uint8Array instances (which doesn't use shared
    memory pool) when TrieNodes store the keys. This change also improves
    the Buffer usage during TrieNode hash calculation.
    
    With this change, average ArrayBuffer usage is down from 1341 bytes to
    44 bytes per entry.

--- apps/hubble/src/utils/crypto.ts
+++ apps/hubble/src/utils/crypto.ts
@@ -1,5 +1,22 @@
+import { blake3 } from '@noble/hashes/blake3';
+
+export const BLAKE3TRUNCATE160_EMPTY_HASH = Buffer.from(blake3(new Uint8Array(), { dkLen: 20 }));
+
 export const sleep = (ms: number) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+};
+
+/**
+ * Compute Blake3-Truncate-160 digest.
+ *
+ * @param msg Message to digest. undefined is treated as empty message.
+ * @return Blake3-Truncate-160 digest
+ */
+export const blake3Truncate160 = (msg: Uint8Array | undefined): Uint8Array => {
+  if (msg === undefined || msg.length === 0) {
+    return BLAKE3TRUNCATE160_EMPTY_HASH;
+  }
+  return blake3(msg, { dkLen: 20 });
 };
