232f7fb2cfd82a42b5df3abbf1ab2f8af05cc650
Paul Fletcher-Hill <pfletcherhill@gmail.com>
2023-03-16 12:47:23 -0400

    fix prune messages job

--- apps/hubble/src/storage/jobs/pruneMessagesJob.ts
+++ apps/hubble/src/storage/jobs/pruneMessagesJob.ts
@@ -21,7 +21,7 @@ export class PruneMessagesJobScheduler {
   }
 
   start(cronSchedule?: string) {
-    this._cronTask = cron.schedule(cronSchedule ?? DEFAULT_PRUNE_MESSAGES_JOB_CRON, this.doJobs);
+    this._cronTask = cron.schedule(cronSchedule ?? DEFAULT_PRUNE_MESSAGES_JOB_CRON, () => this.doJobs());
   }
 
   stop() {
@@ -35,23 +35,28 @@ export class PruneMessagesJobScheduler {
   }
 
   async doJobs(): HubAsyncResult<void> {
-    log.info({}, 'starting doJobs');
+    log.info({}, 'starting prune job');
 
     let finished = false;
+    let pageToken: Uint8Array | undefined;
     do {
-      const fidsPage = await this._engine.getFids();
+      const fidsPage = await this._engine.getFids({ pageToken, pageSize: 100 });
       if (fidsPage.isErr()) {
         return err(fidsPage.error);
       }
       const { fids, nextPageToken } = fidsPage.value;
       if (!nextPageToken) {
         finished = true;
+      } else {
+        pageToken = nextPageToken;
       }
 
       for (const fid of fids) {
         await this._engine.pruneMessages(fid);
       }
     } while (!finished);
+
+    log.info({}, 'finished prune job');
 
     return ok(undefined);
   }
