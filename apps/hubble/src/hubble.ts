117ab6478e0d632835fd1069804bef6f007f7f72
michaelhly <michaelhly@gmail.com>
2023-03-17 18:08:29 -0400

    Build ordering

--- apps/hubble/src/hubble.ts
+++ apps/hubble/src/hubble.ts
@@ -1,14 +1,6 @@
 import * as protobufs from '@farcaster/protobufs';
-import {
-  ContactInfoContent,
-  FarcasterNetwork,
-  GossipAddressInfo,
-  GossipMessage,
-  HubState,
-  IdRegistryEvent,
-  Message,
-  NameRegistryEvent,
-} from '@farcaster/protobufs';
+import { FarcasterNetwork, HubState, IdRegistryEvent, Message, NameRegistryEvent } from '@farcaster/protobufs';
+import { ContactInfoContent, GossipAddressInfo, GossipMessage } from '@farcaster/rpc';
 import {
   HubAsyncResult,
   HubError,
