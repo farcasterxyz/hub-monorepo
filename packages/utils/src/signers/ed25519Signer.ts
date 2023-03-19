b9f3da28d8ca1f58fa8b7590a975b922a61192bb
Tony D'Addeo <tony@merklemanufactory.com>
2023-03-19 12:13:46 -0500

    add comment to getSignerKey

--- packages/utils/src/signers/ed25519Signer.ts
+++ packages/utils/src/signers/ed25519Signer.ts
@@ -9,6 +9,9 @@ export abstract class Ed25519Signer implements Signer {
   /** Signature scheme as defined in protobufs */
   public readonly scheme = SignatureScheme.ED25519;
 
+  /**
+   * Get the 256-bit public key in bytes
+   */
   public abstract getSignerKey(): HubAsyncResult<Uint8Array>;
   public abstract signMessageHash(hash: Uint8Array): HubAsyncResult<Uint8Array>;
 }
--- packages/utils/src/signers/eip712Signer.ts
+++ packages/utils/src/signers/eip712Signer.ts
@@ -10,6 +10,9 @@ export abstract class Eip712Signer implements Signer {
   /** Signature scheme as defined in protobufs */
   public readonly scheme = SignatureScheme.EIP712;
 
+  /**
+   * Get the 160-bit address in bytes.
+   */
   public abstract getSignerKey(): HubAsyncResult<Uint8Array>;
   public abstract signMessageHash(hash: Uint8Array): HubAsyncResult<Uint8Array>;
   public abstract signVerificationEthAddressClaim(claim: VerificationEthAddressClaim): HubAsyncResult<Uint8Array>;
