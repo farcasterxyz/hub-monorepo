65f5203118f8c686708ad5dd09aaaff6000d7d4e
Tony D'Addeo <tony@merklemanufactory.com>
2023-03-19 12:11:10 -0500

    rm signer util fns

--- packages/utils/src/signers/index.ts
+++ packages/utils/src/signers/index.ts
@@ -4,10 +4,3 @@ export * from './ethersEip712Signer';
 export * from './nobleEd25519Signer';
 export * from './signer';
 export * from './types';
-
-import { EthersEip712Signer } from './ethersEip712Signer';
-import { NobleEd25519Signer } from './nobleEd25519Signer';
-
-export const getEd25519Signer = (privateKey: Uint8Array) => new NobleEd25519Signer(privateKey);
-export const getEip712Signer = (...args: ConstructorParameters<typeof EthersEip712Signer>) =>
-  new EthersEip712Signer(...args);
