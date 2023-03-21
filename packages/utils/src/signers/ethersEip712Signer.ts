12c9c40081ad4772062336a8fd03f272b344f5ae
Anthony D'Addeo <amdaddeo@gmail.com>
2023-03-21 17:03:10 -0500

    chore: use subset of ethers Signer class (#716)
    * chore: use subset of ethers Signer class
    
    * use changeset

--- packages/utils/src/signers/ethersEip712Signer.ts
+++ packages/utils/src/signers/ethersEip712Signer.ts
@@ -1,15 +1,14 @@
-import { Signer as EthersSigner } from 'ethers';
 import { eip712 } from '../crypto';
 import { HubAsyncResult } from '../errors';
 import { VerificationEthAddressClaim } from '../verifications';
 import { Eip712Signer } from './eip712Signer';
 
 export class EthersEip712Signer extends Eip712Signer {
-  private readonly _ethersSigner: EthersSigner;
+  private readonly _ethersSigner: eip712.MinimalEthersSigner;
 
-  constructor(typedDataSigner: EthersSigner) {
+  constructor(signer: eip712.MinimalEthersSigner) {
     super();
-    this._ethersSigner = typedDataSigner;
+    this._ethersSigner = signer;
   }
 
   public async getSignerKey(): HubAsyncResult<Uint8Array> {
