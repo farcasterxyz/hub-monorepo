1e4e5ba5226b12261d46c45ad40fdc9e7d90ed35
Paul Fletcher-Hill <1607180+pfletcherhill@users.noreply.github.com>
2023-04-04 23:21:01 -0400

    chore: update tsup configs (#819)
    * update tsup configs
    
    * add more browser options
    
    * add changeset
    
    * update browser versions

--- packages/hub-nodejs/tsup.config.ts
+++ packages/hub-nodejs/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  target: ['node14', 'chrome79', 'edge109', 'firefox102', 'safari12'],
+  target: ['node14'],
   entryPoints: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
