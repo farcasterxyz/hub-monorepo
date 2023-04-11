6ab5ff08b8d0d4dc94dbe9c012cd3cd88de0cfca
Varun Srinivasan <varunsrin@gmail.com>
2023-04-11 14:13:15 -0700

    chore: add peer ids for trusted mainnet hubs from users

--- apps/hubble/src/allowedPeers.mainnet.ts
+++ apps/hubble/src/allowedPeers.mainnet.ts
@@ -5,6 +5,7 @@ export const MAINNET_ALLOWED_PEERS = [
   '12D3KooWMQrf6unpGJfLBmTGy3eKTo4cGcXktWRbgMnfbZLXqBbn', // nemes.farcaster.xyz
   '12D3KooWN9ztVoHYi26NwTDDLNjgBozz479br3pwBuy3oLQVQmhz', // @cassie
   '12D3KooWHiyvASqvhGn9kqsLBemPTAF6UYEaFGaLUpPxnM54KFbG', // @deodad
+  '12D3KooWKXAG1Z212HHrwYaW5NnwBxoXkk3Sr7USzRxMzS9pieZi', // @paden
   '12D3KooWFbnaXtbD5fwbMGq2JRjPaj7C6EpZRgaxC8jdtcX7FJbZ', // @v
   '12D3KooWMDdQaMWCkQ8Gf3C6zdJdMEfFs8R2pw8YQw2HgoY8qhzA', // @adityapk
   '12D3KooWKwUpms7tgUoVsjQ2uV9g93hS2UiMRUfR8f4xjMtsk1Kq', // @pfh
@@ -15,6 +16,6 @@ export const MAINNET_ALLOWED_PEERS = [
   '12D3KooWReH654LePzAE4shSw6wuWqP3i9jSN2jBNswWJu2kiPQ5', // @vern
   '12D3KooWRxgeFVLTAxeVgj4QHPzuT6HaV3Hj93LNZQeRi7gAwUVk', // @stout
   '12D3KooWSii9GJheDmFRBzii43fpzvb3WkmPn62QSoJ8MjvGKMxr', // @mp
-  '12D3KooWHT3zEUaNtyDX4GKHa5WtfymAJpYS3qZ9vifvAKhMQ6kn', // @brianjckim
+  '12D3KooWQ76P8kBUvjNx2Q8EpYDL1BsqXdqZmAkHzTxE1uruuwRd', // @brianjckim
   '12D3KooWRrFF356RALgsPwQQpg68reJL2y94VwKZFogUkM9der1V', // @lndnnft
 ];
