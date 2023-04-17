5cca59faf4479fe6c33db7a629fd153172259479
michaelhly <michaelhly@gmail.com>
2023-04-16 21:22:50 -0400

    Add test for mnemonic account

--- packages/core/src/signers/viemLocalEip712Signer.test.ts
+++ packages/core/src/signers/viemLocalEip712Signer.test.ts
@@ -8,7 +8,7 @@ import { FarcasterNetwork } from '../protobufs';
 import { Wallet } from 'ethers5';
 import { ethersWalletToAccount } from 'viem/ethers';
 import { blake3 } from '@noble/hashes/blake3';
-import { privateKeyToAccount } from 'viem/accounts';
+import { mnemonicToAccount, privateKeyToAccount } from 'viem/accounts';
 import { Hex } from 'viem/dist/types/types';
 
 describe('ViemLocalEip712Signer', () => {
@@ -68,9 +68,61 @@ describe('ViemLocalEip712Signer', () => {
     let signerKey: Uint8Array;
 
     beforeAll(async () => {
-      // Works
       const privateKeyAccount = privateKeyToAccount(Wallet.createRandom().privateKey as Hex);
       signer = new ViemLocalEip712Signer(privateKeyAccount);
+      signerKey = (await signer.getSignerKey())._unsafeUnwrap();
+    });
+
+    describe('instanceMethods', () => {
+      describe('signMessageHash', () => {
+        test('generates valid signature', async () => {
+          const bytes = randomBytes(32);
+          const hash = blake3(bytes, { dkLen: 20 });
+          const signature = (await signer.signMessageHash(hash))._unsafeUnwrap();
+          const recoveredAddress = (await eip712.verifyMessageHashSignature(hash, signature))._unsafeUnwrap();
+          expect(recoveredAddress).toEqual(signerKey);
+        });
+      });
+
+      describe('signVerificationEthAddressClaim', () => {
+        let claim: VerificationEthAddressClaim;
+        let signature: Uint8Array;
+
+        beforeAll(async () => {
+          claim = makeVerificationEthAddressClaim(
+            Factories.Fid.build(),
+            signerKey,
+            FarcasterNetwork.TESTNET,
+            Factories.BlockHash.build()
+          )._unsafeUnwrap();
+          signature = (await signer.signVerificationEthAddressClaim(claim))._unsafeUnwrap();
+        });
+
+        test('succeeds', async () => {
+          expect(signature).toBeTruthy();
+          const recoveredAddress = eip712.verifyVerificationEthAddressClaimSignature(claim, signature)._unsafeUnwrap();
+          expect(recoveredAddress).toEqual(signerKey);
+        });
+
+        test('succeeds when encoding twice', async () => {
+          const claim2: VerificationEthAddressClaim = { ...claim };
+          const signature2 = (await signer.signVerificationEthAddressClaim(claim2))._unsafeUnwrap();
+          expect(signature2).toEqual(signature);
+          expect(bytesToHexString(signature2)).toEqual(bytesToHexString(signature));
+        });
+      });
+    });
+  });
+
+  describe('with mnemonic account', () => {
+    let signer: ViemLocalEip712Signer;
+    let signerKey: Uint8Array;
+
+    beforeAll(async () => {
+      const mnemonicAccount = mnemonicToAccount(
+        'legal winner thank year wave sausage worth useful legal winner thank yellow'
+      );
+      signer = new ViemLocalEip712Signer(mnemonicAccount);
       signerKey = (await signer.getSignerKey())._unsafeUnwrap();
     });
 
