243d7d60ac4180328b8759a6487b309698bf64ef
michaelhly <michaelhly@gmail.com>
2023-04-16 21:20:26 -0400

    use random wallet

--- packages/core/src/signers/viemLocalEip712Signer.test.ts
+++ packages/core/src/signers/viemLocalEip712Signer.test.ts
@@ -3,12 +3,13 @@ import { bytesToHexString } from '../bytes';
 import { eip712 } from '../crypto';
 import { Factories } from '../factories';
 import { VerificationEthAddressClaim, makeVerificationEthAddressClaim } from '../verifications';
-import { ViemLocalEip712Signer } from './ViemLocalEip712Signer';
+import { ViemLocalEip712Signer } from './viemLocalEip712Signer';
 import { FarcasterNetwork } from '../protobufs';
 import { Wallet } from 'ethers5';
 import { ethersWalletToAccount } from 'viem/ethers';
 import { blake3 } from '@noble/hashes/blake3';
-// import { privateKeyToAccount } from 'viem/accounts';
+import { privateKeyToAccount } from 'viem/accounts';
+import { Hex } from 'viem/dist/types/types';
 
 describe('ViemLocalEip712Signer', () => {
   describe('with ethers account', () => {
@@ -16,9 +17,7 @@ describe('ViemLocalEip712Signer', () => {
     let signerKey: Uint8Array;
 
     beforeAll(async () => {
-      const ethersAccount = ethersWalletToAccount(
-        new Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80')
-      );
+      const ethersAccount = ethersWalletToAccount(Wallet.createRandom());
       signer = new ViemLocalEip712Signer(ethersAccount);
       signerKey = (await signer.getSignerKey())._unsafeUnwrap();
     });
@@ -64,56 +63,55 @@ describe('ViemLocalEip712Signer', () => {
     });
   });
 
-  // describe('with private key account', () => {
-  //   let signer: ViemLocalEip712Signer;
-  //   let signerKey: Uint8Array;
+  describe('with private key account', () => {
+    let signer: ViemLocalEip712Signer;
+    let signerKey: Uint8Array;
 
-  //   beforeAll(async () => {
-  //     const privateKeyAccount = privateKeyToAccount(
-  //       '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
-  //     );
-  //     signer = new ViemLocalEip712Signer(privateKeyAccount);
-  //     signerKey = (await signer.getSignerKey())._unsafeUnwrap();
-  //   });
+    beforeAll(async () => {
+      // Works
+      const privateKeyAccount = privateKeyToAccount(Wallet.createRandom().privateKey as Hex);
+      signer = new ViemLocalEip712Signer(privateKeyAccount);
+      signerKey = (await signer.getSignerKey())._unsafeUnwrap();
+    });
 
-  //   describe('instanceMethods', () => {
-  //     describe('signMessageHash', () => {
-  //       test('generates valid signature', async () => {
-  //         const bytes = randomBytes(32);
-  //         const hash = blake3(bytes, { dkLen: 20 });
-  //         const signature = (await signer.signMessageHash(hash))._unsafeUnwrap();
-  //         const recoveredAddress = (await eip712.verifyMessageHashSignature(hash, signature))._unsafeUnwrap();
-  //         expect(recoveredAddress).toEqual(signerKey);
-  //       });
-  //     });
+    describe('instanceMethods', () => {
+      describe('signMessageHash', () => {
+        test('generates valid signature', async () => {
+          const bytes = randomBytes(32);
+          const hash = blake3(bytes, { dkLen: 20 });
+          const signature = (await signer.signMessageHash(hash))._unsafeUnwrap();
+          const recoveredAddress = (await eip712.verifyMessageHashSignature(hash, signature))._unsafeUnwrap();
+          expect(recoveredAddress).toEqual(signerKey);
+        });
+      });
 
-  //     describe('signVerificationEthAddressClaim', () => {
-  //       let claim: VerificationEthAddressClaim;
-  //       let signature: Uint8Array;
+      describe('signVerificationEthAddressClaim', () => {
+        let claim: VerificationEthAddressClaim;
+        let signature: Uint8Array;
 
-  //       beforeAll(async () => {
-  //         claim = makeVerificationEthAddressClaim(
-  //           Factories.Fid.build(),
-  //           signerKey,
-  //           FarcasterNetwork.TESTNET,
-  //           Factories.BlockHash.build()
-  //         )._unsafeUnwrap();
-  //         signature = (await signer.signVerificationEthAddressClaim(claim))._unsafeUnwrap();
-  //       });
+        beforeAll(async () => {
+          claim = makeVerificationEthAddressClaim(
+            Factories.Fid.build(),
+            signerKey,
+            FarcasterNetwork.TESTNET,
+            Factories.BlockHash.build()
+          )._unsafeUnwrap();
+          signature = (await signer.signVerificationEthAddressClaim(claim))._unsafeUnwrap();
+        });
 
-  //       test('succeeds', async () => {
-  //         expect(signature).toBeTruthy();
-  //         const recoveredAddress = eip712.verifyVerificationEthAddressClaimSignature(claim, signature)._unsafeUnwrap();
-  //         expect(recoveredAddress).toEqual(signerKey);
-  //       });
+        test('succeeds', async () => {
+          expect(signature).toBeTruthy();
+          const recoveredAddress = eip712.verifyVerificationEthAddressClaimSignature(claim, signature)._unsafeUnwrap();
+          expect(recoveredAddress).toEqual(signerKey);
+        });
 
-  //       test('succeeds when encoding twice', async () => {
-  //         const claim2: VerificationEthAddressClaim = { ...claim };
-  //         const signature2 = (await signer.signVerificationEthAddressClaim(claim2))._unsafeUnwrap();
-  //         expect(signature2).toEqual(signature);
-  //         expect(bytesToHexString(signature2)).toEqual(bytesToHexString(signature));
-  //       });
-  //     });
-  //   });
-  // });
+        test('succeeds when encoding twice', async () => {
+          const claim2: VerificationEthAddressClaim = { ...claim };
+          const signature2 = (await signer.signVerificationEthAddressClaim(claim2))._unsafeUnwrap();
+          expect(signature2).toEqual(signature);
+          expect(bytesToHexString(signature2)).toEqual(bytesToHexString(signature));
+        });
+      });
+    });
+  });
 });
