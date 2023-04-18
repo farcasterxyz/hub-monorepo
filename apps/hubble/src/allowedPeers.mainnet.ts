a67c7c2d6e6da423d9c4ef4b20c596550c296503
David Furlong <david@deedmob.com>
2023-04-18 11:49:08 -0700

    Update allowedPeers.mainnet.ts (#886)

--- apps/hubble/src/allowedPeers.mainnet.ts
+++ apps/hubble/src/allowedPeers.mainnet.ts
@@ -9,7 +9,7 @@ export const MAINNET_ALLOWED_PEERS = [
   '12D3KooWFbnaXtbD5fwbMGq2JRjPaj7C6EpZRgaxC8jdtcX7FJbZ', // @v
   '12D3KooWMDdQaMWCkQ8Gf3C6zdJdMEfFs8R2pw8YQw2HgoY8qhzA', // @adityapk
   '12D3KooWKwUpms7tgUoVsjQ2uV9g93hS2UiMRUfR8f4xjMtsk1Kq', // @pfh
-  '12D3KooWBWSxYTN1cdjyJqbKzvnaUHf5nEvAm4NfLHQg68QL1czp', // @df
+  '12D3KooWATZKmurjSqWHfcLBgD4oYbNYiAHZ5VPq8W2kNid5TEuL', // @df
   '12D3KooWMnmYq2BDGKJ76w5pFyz2c1ZsK8pMvu6wxyg747j3p679', // @eddy
   '12D3KooWFoPsstYWVH3a4uFpmxksF6nDEsgYizHnPpd9Atw9DRbH', // @gavi
   '12D3KooWKYqmj1bQJChn4hCAhsneg9sRKRd6Zzp3DkVMUL2pjyAB', // @16
