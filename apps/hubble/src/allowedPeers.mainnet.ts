9fd50ac5c7ccb95fb2579cc4b2cf36ce29bffd3c
Tiago Alves <alvesjtiago@gmail.com>
2023-04-29 18:20:27 +0100

    Add eventcaster hub to allowlist

--- apps/hubble/src/allowedPeers.mainnet.ts
+++ apps/hubble/src/allowedPeers.mainnet.ts
@@ -27,4 +27,5 @@ export const MAINNET_ALLOWED_PEERS = [
   '12D3KooWPaHFmDULq5ahsFM7TQ9tC3UmpiC5XUjtYe9xaoaw7iAs', // @les
   '12D3KooWEZbpDyiupf2p9dWREfAigKou5wT4jcvxGVTcdXKXBuMu', // Jam.so
   '12D3KooWEr4WoKduAQg8VJSgwcNsqptbdkSQLw1ceEr8oSBQurJX', // @yanisme
+  '12D3KooWMZK4VeMX3ZARugTuGexXFHNiGHgNFq6XE2vzi88fMgwC', // @event
 ];
