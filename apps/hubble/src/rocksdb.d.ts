be0c3b6ecc6ac9341d832842596bced8acef3f79
Shane da Silva <shane@dasilva.io>
2023-05-09 14:06:05 -0700

    Switch to rocksdb fork with precompiled Linux ARM build
    With more Macs running ARM via Apple Silicon, the significantly slow
    package installation was problematic. Fix by using our fork which
    includes the prebuilt binary to reduce install time.

--- apps/hubble/src/rocksdb.d.ts
+++ apps/hubble/src/rocksdb.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for rocksdb 3.0
-// Project: https://github.com/Level/rocksdb
+// Type definitions for @farcaster/rocksdb 3.0
+// Project: https://github.com/farcasterxyz/rocksdb
 // Definitions by: Meirion Hughes <https://github.com/MeirionHughes>
 //                 Daniel Byrne <https://github.com/danwbyrne>
 //                 Paul Fletcher-Hill <https://github.com/pfletcherhill>
@@ -8,87 +8,92 @@
 
 /// <reference types="node" />
 
-import {
-  AbstractBatch,
-  AbstractChainedBatch,
-  AbstractGetOptions,
-  AbstractIterator,
-  AbstractIteratorOptions,
-  AbstractLevelDOWN,
-  AbstractOpenOptions,
-  AbstractOptions,
-  ErrorCallback,
-  ErrorValueCallback,
-} from 'abstract-leveldown';
+// If we ever start using the upstream rocksdb package again, remove this
+// wrapper `module` declaration. It's only needed because our forked package is
+// scoped to @farcaster.
+declare module '@farcaster/rocksdb' {
+  import {
+    AbstractBatch,
+    AbstractChainedBatch,
+    AbstractGetOptions,
+    AbstractIterator,
+    AbstractIteratorOptions,
+    AbstractLevelDOWN,
+    AbstractOpenOptions,
+    AbstractOptions,
+    ErrorCallback,
+    ErrorValueCallback,
+  } from 'abstract-leveldown';
 
-interface RocksDB extends AbstractLevelDOWN<RocksDB.Bytes, RocksDB.Bytes> {
-  open(cb: ErrorCallback): void;
-  open(options: RocksDB.OpenOptions, cb: ErrorCallback): void;
+  interface RocksDB extends AbstractLevelDOWN<RocksDB.Bytes, RocksDB.Bytes> {
+    open(cb: ErrorCallback): void;
+    open(options: RocksDB.OpenOptions, cb: ErrorCallback): void;
 
-  get(key: RocksDB.Bytes, cb: ErrorValueCallback<RocksDB.Bytes>): void;
-  get(key: RocksDB.Bytes, options: RocksDB.GetOptions, cb: ErrorValueCallback<RocksDB.Bytes>): void;
+    get(key: RocksDB.Bytes, cb: ErrorValueCallback<RocksDB.Bytes>): void;
+    get(key: RocksDB.Bytes, options: RocksDB.GetOptions, cb: ErrorValueCallback<RocksDB.Bytes>): void;
 
-  put(key: RocksDB.Bytes, value: RocksDB.Bytes, cb: ErrorCallback): void;
-  put(key: RocksDB.Bytes, value: RocksDB.Bytes, options: RocksDB.PutOptions, cb: ErrorCallback): void;
+    put(key: RocksDB.Bytes, value: RocksDB.Bytes, cb: ErrorCallback): void;
+    put(key: RocksDB.Bytes, value: RocksDB.Bytes, options: RocksDB.PutOptions, cb: ErrorCallback): void;
 
-  del(key: RocksDB.Bytes, cb: ErrorCallback): void;
-  del(key: RocksDB.Bytes, options: RocksDB.DelOptions, cb: ErrorCallback): void;
+    del(key: RocksDB.Bytes, cb: ErrorCallback): void;
+    del(key: RocksDB.Bytes, options: RocksDB.DelOptions, cb: ErrorCallback): void;
 
-  batch(): AbstractChainedBatch<RocksDB.Bytes, RocksDB.Bytes>;
-  batch(array: AbstractBatch[], cb: ErrorCallback): AbstractChainedBatch<RocksDB.Bytes, RocksDB.Bytes>;
-  batch(
-    array: AbstractBatch[],
-    options: RocksDB.BatchOptions,
-    cb: ErrorCallback
-  ): AbstractChainedBatch<RocksDB.Bytes, RocksDB.Bytes>;
+    batch(): AbstractChainedBatch<RocksDB.Bytes, RocksDB.Bytes>;
+    batch(array: AbstractBatch[], cb: ErrorCallback): AbstractChainedBatch<RocksDB.Bytes, RocksDB.Bytes>;
+    batch(
+      array: AbstractBatch[],
+      options: RocksDB.BatchOptions,
+      cb: ErrorCallback
+    ): AbstractChainedBatch<RocksDB.Bytes, RocksDB.Bytes>;
 
-  approximateSize(start: RocksDB.Bytes, end: RocksDB.Bytes, cb: RocksDB.ErrorSizeCallback): void;
-  compactRange(start: RocksDB.Bytes, end: RocksDB.Bytes, cb: ErrorCallback): void;
-  getProperty(property: string): string;
-  iterator(options?: RocksDB.IteratorOptions): RocksDB.Iterator;
+    approximateSize(start: RocksDB.Bytes, end: RocksDB.Bytes, cb: RocksDB.ErrorSizeCallback): void;
+    compactRange(start: RocksDB.Bytes, end: RocksDB.Bytes, cb: ErrorCallback): void;
+    getProperty(property: string): string;
+    iterator(options?: RocksDB.IteratorOptions): RocksDB.Iterator;
+  }
+
+  declare namespace RocksDB {
+    type Bytes = string | Buffer;
+    type ErrorSizeCallback = (err: Error | undefined, size: number) => void;
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface OpenOptions extends AbstractOpenOptions {}
+
+    interface GetOptions extends AbstractGetOptions {
+      fillCache?: boolean | undefined;
+    }
+
+    interface PutOptions extends AbstractOptions {
+      sync?: boolean | undefined;
+    }
+
+    interface DelOptions extends AbstractOptions {
+      sync?: boolean | undefined;
+    }
+
+    interface BatchOptions extends AbstractOptions {
+      sync?: boolean | undefined;
+    }
+
+    interface IteratorOptions extends AbstractIteratorOptions<Bytes> {
+      fillCache?: boolean | undefined;
+    }
+
+    interface Iterator extends AbstractIterator<Bytes, Bytes> {
+      seek(key: Bytes): void;
+      binding: any;
+      cache: any;
+      finished: any;
+      fastFuture: any;
+    }
+
+    interface Constructor {
+      new (location: string): RocksDB;
+      (location: string): RocksDB;
+      destroy(location: string, cb: ErrorCallback): void;
+      repair(location: string, cb: ErrorCallback): void;
+    }
+  }
+
+  declare const RocksDB: RocksDB.Constructor;
+  export default RocksDB;
 }
-
-declare namespace RocksDB {
-  type Bytes = string | Buffer;
-  type ErrorSizeCallback = (err: Error | undefined, size: number) => void;
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface OpenOptions extends AbstractOpenOptions {}
-
-  interface GetOptions extends AbstractGetOptions {
-    fillCache?: boolean | undefined;
-  }
-
-  interface PutOptions extends AbstractOptions {
-    sync?: boolean | undefined;
-  }
-
-  interface DelOptions extends AbstractOptions {
-    sync?: boolean | undefined;
-  }
-
-  interface BatchOptions extends AbstractOptions {
-    sync?: boolean | undefined;
-  }
-
-  interface IteratorOptions extends AbstractIteratorOptions<Bytes> {
-    fillCache?: boolean | undefined;
-  }
-
-  interface Iterator extends AbstractIterator<Bytes, Bytes> {
-    seek(key: Bytes): void;
-    binding: any;
-    cache: any;
-    finished: any;
-    fastFuture: any;
-  }
-
-  interface Constructor {
-    new (location: string): RocksDB;
-    (location: string): RocksDB;
-    destroy(location: string, cb: ErrorCallback): void;
-    repair(location: string, cb: ErrorCallback): void;
-  }
-}
-
-declare const RocksDB: RocksDB.Constructor;
-export default RocksDB;
