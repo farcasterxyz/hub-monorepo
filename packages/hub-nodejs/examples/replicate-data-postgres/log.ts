542f6d82e434f34c4247bbbf84d4770cb130304d
Shane da Silva <shane@dasilva.io>
2023-05-09 18:32:52 -0700

    Streamline replication example further
    Instead of requiring the user to run `yarn install` followed by `yarn
    start`, we simply have them run `docker compose up` to start both the
    app and Postgres together. This is much easier and reduces the need for
    them to install anything else besides Docker. It also makes cleaning up
    the example easier.

--- packages/hub-nodejs/examples/replicate-data-postgres/log.ts
+++ packages/hub-nodejs/examples/replicate-data-postgres/log.ts
@@ -1,5 +1,12 @@
 import { pino } from 'pino';
 
-export const log = pino();
+export const log = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+});
 
 export type Logger = pino.Logger;
