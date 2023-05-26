f8b628932094c5ebe3a0b292bcca32ec404a63b9
Tony D'Addeo <tony@merklemanufactory.com>
2023-05-26 15:18:02 -0500

    add ViemLocalEip712Signer to exports

--- packages/core/src/signers/index.ts
+++ packages/core/src/signers/index.ts
@@ -3,5 +3,6 @@ export * from './eip712Signer';
 export * from './ethersEip712Signer';
 export * from './ethersV5Eip712Signer';
 export * from './nobleEd25519Signer';
+export * from './viemLocalEip712Signer';
 export * from './signer';
 export * from './types';
