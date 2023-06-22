fb93aef86204a53053bb50b0f493adde1c1cf678
Sanjay Raveendran <sanjayprabhu@gmail.com>
2023-06-21 19:12:38 -0700

    feat: add support for verifying username proofs

--- packages/core/src/crypto/eip712.test.ts
+++ packages/core/src/crypto/eip712.test.ts
@@ -6,6 +6,7 @@ import { bytesToHexString, hexStringToBytes } from '../bytes';
 import { Factories } from '../factories';
 import { VerificationEthAddressClaim } from '../verifications';
 import * as eip712 from './eip712';
+import { UserNameProofClaim } from './eip712';
 
 const wallet = Wallet.createRandom();
 const fid = Factories.Fid.build();
@@ -61,5 +62,23 @@ describe('signMessageHash', () => {
     expect(signature.isOk()).toBeTruthy();
     const recoveredAddress = eip712.verifyMessageHashSignature(hash, signature._unsafeUnwrap());
     expect(recoveredAddress).toEqual(ok(hexStringToBytes(wallet.address)._unsafeUnwrap()));
+  });
+});
+
+describe('verifyUserNameProof', () => {
+  test('succeeds for a known proof', async () => {
+    const nameProof: UserNameProofClaim = {
+      owner: '0x8773442740c17c9d0f0b87022c722f9a136206ed',
+      name: 'farcaster',
+      timestamp: 1628882891,
+    };
+    const signature = hexStringToBytes(
+      '0xb7181760f14eda0028e0b647ff15f45235526ced3b4ae07fcce06141b73d32960d3253776e62f761363fb8137087192047763f4af838950a96f3885f3c2289c41b'
+    );
+    expect(signature.isOk()).toBeTruthy();
+    const recoveredAddress = eip712.verifyUserNameProof(nameProof, signature._unsafeUnwrap());
+    expect(recoveredAddress).toEqual(
+      ok(hexStringToBytes('0xBc5274eFc266311015793d89E9B591fa46294741')._unsafeUnwrap())
+    );
   });
 });
