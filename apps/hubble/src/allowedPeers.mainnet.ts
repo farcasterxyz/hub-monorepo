49285ce9df1d3b3fa821c0c8d09251f5794e378f
Federico Castelli <31770652+fedecastelli@users.noreply.github.com>
2023-06-29 14:41:21 +0200

    Update allowedPeers.mainnet.ts
    Add my custom peerId to be allowed to use Farcaster Hub on Mainnet

--- apps/hubble/src/allowedPeers.mainnet.ts
+++ apps/hubble/src/allowedPeers.mainnet.ts
@@ -46,4 +46,5 @@ export const MAINNET_ALLOWED_PEERS = [
   '12D3KooWRDvjugV5fhkTA2BBmrLXmKyBXLe4V7YJyMXyLQGj1PAM', // @rysiman
   '12D3KooWNLshuXk4x2gacFTMEJjMGGFxPjpHg5KcV88KoqJP7dDR', // @bhgomes
   '12D3KooWS39hrt41rMuD5ZfPFMxUVycxVwz7w15CxZVhxP4AZDGr', // @rozhnovskiyigor
+  '12D3KooWHXZkdEYWb5RCajv7YbffasjYNSEP1U68DMR1JeZ4QiXH', // @fedecastelli
 ];
