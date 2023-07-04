8be60b95f42425732cf4d18ea74b1ed56a063599
dgct <d@tree.casa>
2023-07-05 01:19:47 +0200

    feat: add zkbio to mainnet allowed peers

--- apps/hubble/src/allowedPeers.mainnet.ts
+++ apps/hubble/src/allowedPeers.mainnet.ts
@@ -21,6 +21,7 @@ export const MAINNET_ALLOWED_PEERS = [
   '12D3KooWPNDmUeNiGdCdoHp8iAf8Uay3c2C9n5QVygd3J1hfv65w', // @sanjay
   '12D3KooWFZwTP5bUZbQViTk79i2DZujHBBjPpAZyDwyrkPb79eAL', // @molo
   '12D3KooWHK4EXZ33nVFLSCLRuFNUK74v72eZEcRKoHTPbX9Nove7', // @neynar
+  '12D3KooWF4mnESYMQmxofQEU2g5M666RN4Yv8VwYQupnAZ7crGSn', // @zkbio
   '12D3KooWDMXc4uVXr8zWGujaRuzhDnTBBHkTsJ58WimB8QU4cgos', // @nj
   '12D3KooWCZBZAirRXA2cecgud2zC7T5iN4hwr9nf7VAn9d8u9px2', // @rss3
   '12D3KooWJwBPFDdCkxWqVE6tCWPs4sDx2t4Pgy2cCewqKzLTRHgr', // @cryptobenkei
