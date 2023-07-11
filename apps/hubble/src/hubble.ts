farcasterxyz/hub-monorepo

commit d5d65bdc6207d78ac07dc21fcc0816c06812fd1b

Prevent connecting to peers not in allowed peer list (#1102)
We needed to add some additional logic to ensure we didn't connect to
peers not in the allowlist.

apps/hubble/src/hubble.ts
@@ -946,6 +946,12 @@ export class Hub implements HubInterface {
   }
 
   async isValidPeer(ourPeerId: PeerId, message: ContactInfoContent) {
+    const peerId = ourPeerId.toString();
+    if (MAINNET_ALLOWED_PEERS?.length && !MAINNET_ALLOWED_PEERS.includes(peerId)) {
+      log.warn(`Peer ${ourPeerId.toString()} is not in the allowed peers list`);
+      return false;
+    }
+
     const theirVersion = message.hubVersion;
     const theirNetwork = message.network;
 

commit f335b3d4c09c96903a3a5e47cf203d8ba199d7c1

Fix allowed peer IDs behavior
This was broken by d5d65bdc.

apps/hubble/src/hubble.ts
@@ -210,6 +210,7 @@ export class Hub implements HubInterface {
   private contactTimer?: NodeJS.Timer;
   private rocksDB: RocksDB;
   private syncEngine: SyncEngine;
+  private allowedPeerIds: string[] = [];
 
   private pruneMessagesJobScheduler: PruneMessagesJobScheduler;
   private periodSyncJobScheduler: PeriodicSyncJobScheduler;
@@ -295,6 +296,13 @@ export class Hub implements HubInterface {
         this.rocksDB,
         this.ethRegistryProvider,
       );
+    }
+
+    this.allowedPeerIds = this.options.allowedPeers || [];
+    if (this.options.network === FarcasterNetwork.MAINNET) {
+      // Mainnet is right now resitrcited to a few peers
+      // Append and de-dup the allowed peers
+      this.allowedPeerIds = [...new Set([...(this.allowedPeerIds ?? []), ...MAINNET_ALLOWED_PEERS])];
     }
   }
 
@@ -404,19 +412,12 @@ export class Hub implements HubInterface {
       this.updateNameRegistryEventExpiryJobWorker.start();
     }
 
-    let allowedPeerIdStrs = this.options.allowedPeers;
-    if (this.options.network === FarcasterNetwork.MAINNET) {
-      // Mainnet is right now resitrcited to a few peers
-      // Append and de-dup the allowed peers
-      allowedPeerIdStrs = [...new Set([...(allowedPeerIdStrs ?? []), ...MAINNET_ALLOWED_PEERS])];
-    }
-
     await this.gossipNode.start(this.options.bootstrapAddrs ?? [], {
       peerId: this.options.peerId,
       ipMultiAddr: this.options.ipMultiAddr,
       announceIp: this.options.announceIp,
       gossipPort: this.options.gossipPort,
-      allowedPeerIdStrs,
+      allowedPeerIdStrs: this.allowedPeerIds,
     });
 
     this.registerEventHandlers();
@@ -947,7 +948,7 @@ export class Hub implements HubInterface {
 
   async isValidPeer(ourPeerId: PeerId, message: ContactInfoContent) {
     const peerId = ourPeerId.toString();
-    if (MAINNET_ALLOWED_PEERS?.length && !MAINNET_ALLOWED_PEERS.includes(peerId)) {
+    if (this.allowedPeerIds?.length && !this.allowedPeerIds.includes(peerId)) {
       log.warn(`Peer ${ourPeerId.toString()} is not in the allowed peers list`);
       return false;
     }