160158e31433b141ef51f46540671907c5769b91
zachterrell57 <zachterrell@gmail.com>
2023-07-11 22:58:36 -0700

    add rome formatting

--- packages/hub-nodejs/examples/replicate-data-postgres/migrations/002_add_hash_to_links.ts
+++ packages/hub-nodejs/examples/replicate-data-postgres/migrations/002_add_hash_to_links.ts
@@ -1,13 +1,13 @@
-import { Kysely, sql } from 'kysely';
-import { Database } from '../db';
+import { Kysely, sql } from "kysely";
+import { Database } from "../db";
 
 export const up = async (db: Kysely<Database>) => {
   await db.schema
-    .alterTable('links')
-    .addColumn('hash', sql`bytea`, (col) => col.notNull())
+    .alterTable("links")
+    .addColumn("hash", sql`bytea`, (col) => col.notNull())
     .execute();
 };
 
 export const down = async (db: Kysely<Database>) => {
-  await db.schema.alterTable('links').dropColumn('hash').execute();
+  await db.schema.alterTable("links").dropColumn("hash").execute();
 };
