937fdd62d7ebdc31acdddda445a07bea4381d64e
Manan <manan19@gmail.com>
2023-07-13 20:06:11 -0700

    chore: just add 3 for now

--- apps/hubble/src/allowedPeers.mainnet.ts
+++ apps/hubble/src/allowedPeers.mainnet.ts
@@ -30,8 +30,6 @@ export const MAINNET_ALLOWED_PEERS = [
   "12D3KooWHK4EXZ33nVFLSCLRuFNUK74v72eZEcRKoHTPbX9Nove7", // @neynar Hub 1
   "12D3KooWB17LnSuiMuRAfGbsEky1JV2cdN2FnafdPtz2C5sdE991", // @neynar Hub 2
   "12D3KooWDWmbsoWZf3edTqJHbx3sZStbgoARKXVQ2zBDUL1BcVLw", // @neynar Hub 3
-  "12D3KooWE7qQ6D216GkZX65ihbzJk2YMYsy35sqe7dmi3nDafRBK", // @neynar Hub 4
-  "12D3KooWLqpt1MJXVTWUFCWWJS5xA9gN9DNnCRv5YgT3PKeNhz8z", // @neynar Hub 5
   "12D3KooWDMXc4uVXr8zWGujaRuzhDnTBBHkTsJ58WimB8QU4cgos", // @nj
   "12D3KooWCZBZAirRXA2cecgud2zC7T5iN4hwr9nf7VAn9d8u9px2", // @rss3
   "12D3KooWJwBPFDdCkxWqVE6tCWPs4sDx2t4Pgy2cCewqKzLTRHgr", // @cryptobenkei
