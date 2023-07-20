f34acea2346e79457e04e4f920bd917d1897fabe
Aditya Kulkarni <adityapk@gmail.com>
2023-07-20 12:48:31 -0500

    add test

--- apps/hubble/src/network/utils/networkConfig.test.ts
+++ apps/hubble/src/network/utils/networkConfig.test.ts
@@ -5,6 +5,21 @@ import { applyNetworkConfig } from "./networkConfig.js";
 describe("networkConfig", () => {
   const network = 2;
 
+  test("no change", () => {
+    const existingPeerIds = ["1", "2", "3"];
+
+    const networkConfig = {
+      network,
+      allowedPeers: ["1", "2", "3"],
+      deniedPeers: [],
+      minAppVersion: APP_VERSION,
+    };
+
+    const result = applyNetworkConfig(networkConfig, existingPeerIds, network);
+    expect(result.allowedPeerIds).toEqual(["1", "2", "3"]);
+    expect(result.shouldExit).toEqual(false);
+  });
+
   test("add peerIDs", () => {
     const existingPeerIds = ["1", "2", "3"];
 
