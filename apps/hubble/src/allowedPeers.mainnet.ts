7d7e9a5e021512dd664591a24fae94fe4b48f0a8
Alizyan <104369242+Alizyan4@users.noreply.github.com>
2023-07-21 17:34:05 +0300

    Update allowedPeers.mainnet.ts

--- apps/hubble/src/allowedPeers.mainnet.ts
+++ apps/hubble/src/allowedPeers.mainnet.ts
@@ -52,6 +52,7 @@ export const MAINNET_ALLOWED_PEERS = [
   "12D3KooWDXSemEhSAidFCZT1HyRhTHayunCokg68VSuwStf8UEU5", // @98967eth
   "12D3KooWHcViME8KUZXo1wStoRF2jmg4edWidexGy5utVyoxNNnn", // @parthkohli
   "12D3KooWJtEEh7Bcx9T2oKUpcMb9K8xuMFFk5BQNNdJ63Rsx853C", // Jam.so Hub 4
+  "12D3KooWNjYPqnxbFEbihnrumV1pSTKouYxMC853LDsPqYyysByM", // @Alizyan
   "12D3KooWH3FAGri9Ki4j7xBBjghav9nRTyu8jVVBsRh55odGxraM", // @kencodes
   "12D3KooWQ8fFUMipLC4Fm6DLN9vWQLX5XBbiewM1N5Vt6QxsPVxn", // @thezluf
   "12D3KooWPsSJu7jPS2UcroV4R1Ed1VJKVBxBkuNs4haDTEUnYBvm", // @sheldon
