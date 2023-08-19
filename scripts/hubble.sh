e77ca0b83e5245aac003e090acc48f016acea578
Aditya Kulkarni <adityapk@gmail.com>
2023-08-19 18:35:56 -0500

    grafana

--- scripts/hubble.sh
+++ scripts/hubble.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
 # Define the version of this script
-CURRENT_VERSION="1.0"
+CURRENT_VERSION="1"
 
-# URL where the latest version of the script is hosted
-SCRIPT_URL="https://example.com/myscript.sh"
+# Fetch the @latest docker-compose file
+REPO="farcasterxyz/hub-monorepo"
+API_BASE="https://api.github.com/repos/$REPO"
+LATEST_TAG="@latest"
+
+DOCKER_COMPOSE_FILE_PATH="apps/hubble/docker-compose.yml"
+SCRIPT_FILE_PATH="scripts/hubble.sh"
+
 
 # Function to check and upgrade the script
+
 self_upgrade() {
     echo "Checking for newer version..."
 
-    # Fetch the latest version of the script
+    # Fetch the commit SHA associated with the @latest tag
+    LATEST_COMMIT_SHA=$(curl -sS "$API_BASE/git/refs/tags/$LATEST_TAG" | grep sha | head -n 1 | cut -d '"' -f 4)
+
+    # If there's no such tag, exit
+    if [ -z "$LATEST_COMMIT_SHA" ]; then
+        echo "No @latest tag found."
+        exit 1
+    fi
+
+    # Fetch the script file associated with the latest tag
+    LATEST_FILE_CONTENT=$(curl -sS "$API_BASE/contents/$SCRIPT_FILE_PATH?ref=$LATEST_COMMIT_SHA")
+    DOWNLOAD_URL=$(echo "$LATEST_FILE_CONTENT" | grep download_url | cut -d '"' -f 4)
+
+    if [ -z "$DOWNLOAD_URL" ]; then
+        echo "Failed to fetch the script."
+        exit 1
+    fi
+
+    # Download the script to a temporary location
     TMP_FILE=$(mktemp)
-    if curl -s -o "$TMP_FILE" "$SCRIPT_URL"; then
+    if curl -sS -o "$TMP_FILE" "$DOWNLOAD_URL"; then
         # Extract the version from the fetched script
         LATEST_VERSION=$(grep -oP '^CURRENT_VERSION="\K[^"]+' "$TMP_FILE")
 
@@ -24,21 +49,17 @@ self_upgrade() {
             echo "Upgrade complete. Restarting with new version..."
             exec "$0" "$@"
         else
-            echo "You have the latest version ($CURRENT_VERSION)."
+            echo "✅ Script version ($CURRENT_VERSION)."
+            rm -f "$TMP_FILE"  # Clean up temporary file if no upgrade was needed
         fi
     else
         echo "Failed to check for newer version. Please try again later."
+        rm -f "$TMP_FILE"  # Clean up temporary file on failure
+        exit 1
     fi
-
-    rm -f "$TMP_FILE"  # Clean up temporary file
 }
 
 
-# Fetch the @latest docker-compose file
-REPO="farcasterxyz/hub-monorepo"
-FILE_PATH="apps/hubble/docker-compose.yml"
-API_BASE="https://api.github.com/repos/$REPO"
-LATEST_TAG="@latest"
 
 fetch_latest_docker_compose() {
     # Fetch the commit SHA associated with the @latest tag
@@ -51,7 +72,7 @@ fetch_latest_docker_compose() {
     fi
 
     # Fetch the docker-compose.yml file associated with the latest tag
-    LATEST_FILE_CONTENT=$(curl -sS "$API_BASE/contents/$FILE_PATH?ref=$LATEST_COMMIT_SHA")
+    LATEST_FILE_CONTENT=$(curl -sS "$API_BASE/contents/$DOCKER_COMPOSE_FILE_PATH?ref=$LATEST_COMMIT_SHA")
     DOWNLOAD_URL=$(echo "$LATEST_FILE_CONTENT" | grep download_url | cut -d '"' -f 4)
 
     if [ -z "$DOWNLOAD_URL" ]; then
@@ -79,7 +100,7 @@ validate_and_store() {
 
 
         if [[ $lower_response == *'"result":"'$lower_expected_chain_id'"'* ]]; then
-            echo "$3=$RPC_URL" >> .script.env
+            echo "$3=$RPC_URL" >> .env
             break
         else
             echo "!!! Invalid !!!"
@@ -121,30 +142,46 @@ write_env_file() {
 }
 
 ## Configure Grafana
+## Configure Grafana
 setup_grafana() {
     local grafana_url="http://127.0.0.1:3000"
     local credentials="admin:admin"
-    local response
+    local response dashboard_uid
 
     add_datasource() {
         response=$(curl -s -o /dev/null -w "%{http_code}" -X "POST" "$grafana_url/api/datasources" \
-            -u "$credentials" \
-            -H "Content-Type: application/json" \
-            --data-binary '{
-               "name":"Graphite",
-               "type":"graphite",
-               "url":"http://statsd:80",
-               "access":"proxy"
-            }')
+                -u "$credentials" \
+                -H "Content-Type: application/json" \
+                --data-binary '{
+            "name":"Graphite",
+            "type":"graphite",
+            "url":"http://statsd:80",
+            "access":"proxy"
+        }')
+
+        # Handle if the datasource already exists
+        if [[ "$response" == "409" ]]; then
+            echo "Datasource 'Graphite' already exists. Skipping..."
+            response="200"
+        fi
     }
 
-    # Step 1: Start statsd and grafana
-    docker compose up -d statsd grafana
+    delete_dashboard() {
+        dashboard_uid=$(curl -s "$grafana_url/api/search?query=Hub Dashboard" -u "$credentials" | \
+                grep -o '"uid":"[^"]*"' | \
+            awk -F '"' '{print $4}')
+        if [[ "$dashboard_uid" ]]; then
+            curl -s -X "DELETE" "$grafana_url/api/dashboards/uid/$dashboard_uid" -u "$credentials"
+        fi
+    }
+
+    # Step 1: Restart statsd and grafana if they are running, otherwise start them
+    docker compose restart statsd grafana || docker compose up -d statsd grafana
 
     # Step 2: Wait for Grafana to be ready
     echo "Waiting for Grafana to be ready..."
-    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $grafana_url/api/health)" != "200" ]]; do 
-        sleep 5; 
+    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $grafana_url/api/health)" != "200" ]]; do
+        sleep 5;
     done
     echo "Grafana is ready."
 
@@ -161,37 +198,41 @@ setup_grafana() {
 
         # Retry adding the data source with the new credentials
         add_datasource
-        
+
         if [[ "$response" != "200" ]]; then
             echo "Failed to add data source with provided credentials. Exiting."
             return 1
         fi
     fi
 
-    # Step 4: Import the dashboard. Assuming the dashboard JSON is in a file named "dashboard.json"
+    # Step 4: Delete the dashboard if it exists
+    delete_dashboard
+
+    # Step 5: Import the dashboard. Assuming the dashboard JSON is in a file named "grafana-dashboard.json"
     curl -X "POST" "$grafana_url/api/dashboards/db" \
-         -u "$credentials" \
-         -H "Content-Type: application/json" \
-         --data-binary @grafana-dashboard.json
+        -u "$credentials" \
+        -H "Content-Type: application/json" \
+        --data-binary @grafana-dashboard.json
 }
 
+
 # Check the command-line argument for 'self-upgrade'
-if [ "$1" == "self-upgrade" ]; then
+if [ "$1" == "upgrade" ]; then
     self_upgrade
+
+    # Ensure the ~/hubble directory exists
+    if [ ! -d ~/hubble ]; then
+        mkdir -p ~/hubble || { echo "Failed to create ~/hubble directory."; exit 1; }
+    fi
+
+    # Ensure the script runs in the ~/hubble directory
+    cd ~/hubble || { echo "Failed to switch to ~/hubble directory."; exit 1; }
+
+    # Update the env file if needed
+    write_env_file
+
+    # Fetch the latest docker-compose.yml
+    fetch_latest_docker_compose
+
     exit 0
 fi
-
-# The rest of your script logic goes here
-echo "hubble.sh version $CURRENT_VERSION"
-
-# Ensure the ~/hubble directory exists
-if [ ! -d ~/hubble ]; then
-    mkdir -p ~/hubble || { echo "Failed to create ~/hubble directory."; exit 1; }
-fi
-
-# Ensure the script runs in the ~/hubble directory
-cd ~/hubble || { echo "Failed to switch to ~/hubble directory."; exit 1; }
-
-write_env_file
-
-fetch_latest_docker_compose
