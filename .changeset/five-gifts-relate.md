68135f9d4d20c6f474d698751e81955bff6fe3d3
Aditya Kulkarni <adityapk@gmail.com>
2023-10-10 12:52:56 -0500

    fix: Persist grafana data across restart

---
+++ .changeset/five-gifts-relate.md
@@ -0,0 +1,5 @@
+---
+"@farcaster/hubble": patch
+---
+
+fix: Persist grafana container data
--- scripts/hubble.sh
+++ scripts/hubble.sh
@@ -199,6 +199,10 @@ write_env_file() {
 }
 
 ensure_grafana() {
+      # Create a grafana data directory if it doesn't exist
+      mkdir -p grafana/data
+      chmod 777 grafana/data
+
       if $COMPOSE_CMD ps statsd 2>&1 >/dev/null; then
           if $COMPOSE_CMD ps statsd | grep -q "Up"; then
               $COMPOSE_CMD restart statsd grafana
