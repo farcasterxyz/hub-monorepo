afa80c7482251184a8a3ac98e886d2d7f2e0f6cd
Sanjay Raveendran <sanjayprabhu@gmail.com>
2024-02-03 14:00:36 -0800

    fix: Reduce sync frequency

--- apps/hubble/src/network/sync/periodicSyncJob.ts
+++ apps/hubble/src/network/sync/periodicSyncJob.ts
@@ -11,7 +11,7 @@ const log = logger.child({
 type SchedulerStatus = "started" | "stopped";
 
 // Every 2 minutes, at 00:45 seconds, to avoid clashing with the prune job
-const DEFAULT_PERIODIC_SYNC_JOB_CRON = "45 */2 * * * *";
+const DEFAULT_PERIODIC_SYNC_JOB_CRON = "45 * */2 * * *";
 
 export class PeriodicSyncJobScheduler {
   private _hub: Hub;
