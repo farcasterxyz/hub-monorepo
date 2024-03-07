ab267eaf346df0db53f8e49f8ae8015b9e8839b9
Aditya Kulkarni <adityapk@gmail.com>
2024-03-07 16:31:28 -0600

    test

--- apps/hubble/src/storage/stores/store.ts
+++ apps/hubble/src/storage/stores/store.ts
@@ -484,14 +484,24 @@ export abstract class Store<TAdd extends Message, TRemove extends Message> {
         } else {
           // If the existing remove has a lower order than the new message, retrieve the full
           // TRemove message and delete it as part of the RocksDB transaction
-          const existingRemove = await getMessage<TRemove>(
-            this._db,
-            // biome-ignore lint/style/noNonNullAssertion: legacy code, avoid using ignore for new code
-            message.data!.fid,
-            this._postfix,
-            removeTsHash.value,
+          const existingRemove = await ResultAsync.fromPromise(
+            getMessage<TRemove>(
+              this._db,
+              // biome-ignore lint/style/noNonNullAssertion: legacy code, avoid using ignore for new code
+              message.data!.fid,
+              this._postfix,
+              removeTsHash.value,
+            ),
+            (e) => e,
           );
-          conflicts.push(existingRemove);
+          if (existingRemove.isOk()) {
+            conflicts.push(existingRemove.value);
+          } else {
+            logger.warn(
+              { message, tsHash, removeTsHash, error: existingRemove.error },
+              "Message's ts_hash exists but message not found in store",
+            );
+          }
         }
       }
     }
