86cfa4f5517379dcda5328348b90f24f1f778964
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-04-06 17:27:11 +0200

    fix: add missing db and redis exports to hub-shuttle (#1876)

--- packages/hub-shuttle/src/index.ts
+++ packages/hub-shuttle/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./shuttle/interfaces";
 export * from "./shuttle/db";
+export * from "./shuttle/redis";
+export * from "./shuttle/hub";
 export * from "./shuttle/hubSubscriber";
 export * from "./shuttle/hubEventProcessor";
 export * from "./shuttle/messageProcessor";
