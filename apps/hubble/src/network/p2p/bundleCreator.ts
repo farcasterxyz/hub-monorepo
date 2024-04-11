397e845d638d4973f2cc5f8822c21c7d593afeec
Aditya Kulkarni <adityapk@gmail.com>
2024-04-11 11:15:20 -0500

    bundling tests

--- apps/hubble/src/network/p2p/bundleCreator.ts
+++ apps/hubble/src/network/p2p/bundleCreator.ts
@@ -1,22 +1,29 @@
-import { HubResult, Message, MessageBundle } from "@farcaster/hub-nodejs";
+import { Message, MessageBundle } from "@farcaster/hub-nodejs";
 import { GossipPublishResult, LibP2PNode } from "./gossipNodeWorker.js";
 import { blake3Truncate160 } from "../../utils/crypto.js";
 
-const MAX_BUNDLE_SIZE = 100;
+const MAX_BUNDLE_SIZE = 256;
+const DEFAULT_BUNDLE_TIME_MS = 1000;
 
 export class BundleCreator {
+  private _maxBundleSize: number;
+  private _defaultBundleTimeMs: number;
+
   private _currentBundle: Message[] = [];
   private _bundleIncomingMessages = false;
   private _libp2pNode: LibP2PNode;
 
-  constructor(libp2pNode: LibP2PNode) {
+  constructor(libp2pNode: LibP2PNode, maxBundleSize = MAX_BUNDLE_SIZE, defaultBundleTimeMs = DEFAULT_BUNDLE_TIME_MS) {
     this._libp2pNode = libp2pNode;
+    this._maxBundleSize = maxBundleSize;
+    this._defaultBundleTimeMs = defaultBundleTimeMs;
+
     this.updateIfBundling();
 
     setInterval(async () => {
       this.updateIfBundling();
       await this.broadcastBundle();
-    }, 1000);
+    }, this._defaultBundleTimeMs);
   }
 
   // Use the % chance of bundling incoming messages to determine whether to bundle incoming messages
@@ -39,7 +46,7 @@ export class BundleCreator {
       this._currentBundle = [];
 
       // Calculate the hash of the bundle
-      const allHashes = Buffer.concat(bundle.map((message) => message.hash));
+      const allHashes = Buffer.concat(bundle.map((message) => message.hash ?? new Uint8Array()));
       const bundleHash = blake3Truncate160(allHashes);
 
       const messageBundle = MessageBundle.create({
@@ -57,12 +64,12 @@ export class BundleCreator {
     if (this._bundleIncomingMessages) {
       this._currentBundle.push(message);
 
-      if (this._currentBundle.length >= MAX_BUNDLE_SIZE) {
+      if (this._currentBundle.length >= this._maxBundleSize) {
         await this.broadcastBundle();
         this._currentBundle = [];
       }
 
-      return { bundled: true, publishResults: [] };
+      return { bundled: true, publishResults: undefined };
     } else {
       const publishResults = await this._libp2pNode.broadcastMessage(message);
       return { bundled: false, publishResults };
