751010b4be31a85b2e011bd2bf1338548bab9050
Sanjay Raveendran <sanjayprabhu@gmail.com>
2024-04-10 22:14:22 -0700

    test

--- packages/hub-shuttle/tsup.config.ts
+++ packages/hub-shuttle/tsup.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   target: ["node14", "chrome79", "edge109", "firefox102", "safari12"],
-  entryPoints: ["./src/**/*.ts"],
-  esbuildOptions(options, context) {
-    options.outbase = "./";
-  },
-  bundle: false,
+  entryPoints: ["./src/index.ts"],
+  // esbuildOptions(options, context) {
+  //   options.outbase = "./";
+  // },
+  // bundle: false,
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
