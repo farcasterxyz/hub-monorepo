f5fe31623ce0c4e886495c65f64f822f512c8242
Sanjay Raveendran <sanjayprabhu@gmail.com>
2024-07-31 15:06:47 -0700

    fix: Include peerid in bootstrap multiaddr

---
+++ .changeset/metal-meals-beg.md
@@ -0,0 +1,5 @@
+---
+"@farcaster/hubble": patch
+---
+
+fix: Include peerid in bootstrap multiaddr
--- apps/hubble/src/bootstrapPeers.mainnet.ts
+++ apps/hubble/src/bootstrapPeers.mainnet.ts
@@ -1,6 +1,6 @@
 // List of bootstrap peers for mainnet
 export const MAINNET_BOOTSTRAP_PEERS = [
-  "/dns/hoyt.farcaster.xyz/tcp/2282",
-  "/dns/lamia.farcaster.xyz/tcp/2282",
-  "/dns/bootstrap.neynar.com/tcp/2282",
+  "/dns/hoyt.farcaster.xyz/tcp/2282/p2p/12D3KooWRnSZUxjVJjbSHhVKpXtvibMarSfLSKDBeMpfVaNm1Joo",
+  "/dns/lamia.farcaster.xyz/tcp/2282/p2p/12D3KooWJECuSHn5edaorpufE9ceAoqR5zcAuD4ThoyDzVaz77GV",
+  "/dns/bootstrap.neynar.com/tcp/2282/p2p/12D3KooWNsC2vzuHdKDfSM6xnMZwMjWK8zZCYHyLXuhRMeVRebGK",
 ];
