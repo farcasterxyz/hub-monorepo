0152ae6feb9635e0f30b059786c496966fa08c75
Pavel <pashu13@list.ru>
2024-12-02 08:12:10 +0300

    index.ts

--- packages/shuttle/src/shuttle/index.ts
+++ packages/shuttle/src/shuttle/index.ts
@@ -17,7 +17,7 @@ export type ProcessResult = {
   skipped: boolean;
 };
 
-// Implement this interface in your app to handle messages. The package currently provides the following gurantees:
+// Implement this interface in your app to handle messages. The package currently provides the following guarantees:
 // - Messages can be assumed to be processes in the same order as they were recieved by the hub as long as wasMissed in false
 // - if wasMissed is true, then the package provides no guarantees about the ordering (it is possible to receive an add
 //      after a remove, your app needs to handle the CRDT resolution). We will provide a way to handle this in the future.
