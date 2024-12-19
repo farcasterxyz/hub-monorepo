aa1f9fb7ea54df4c5e7f340da64b57ec693957a8
Ty Book <ty@neynar.com>
2024-12-19 14:33:50 -0500

    Baseline changes for subrepo Shuttle to work with Neynar's Yarn workspace setup
    Signed-off-by: topocount <17910833+topocount@users.noreply.github.com>

--- packages/shuttle/tsup.config.ts
+++ packages/shuttle/tsup.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   target: ["node14", "chrome79", "edge109", "firefox102", "safari12"],
   entryPoints: ["./src/index.ts"],
   format: ["esm", "cjs"],
-  dts: true,
+  dts: false,
   clean: true,
   shims: true,
+  sourcemap: true,
 });
