cb0357db2a38280815c8b8581a541d77473e7986
kewe63 <86300262+Kewe63@users.noreply.github.com>
2025-10-14 21:41:18 +0300

    Update generate-protos.sh
    upgrade

--- generate-protos.sh
+++ generate-protos.sh
@@ -1,32 +1,57 @@
-PROTO_REPO=https://github.com/farcasterxyz/snapchain
-PROTO_PATH=src/proto
-PROTO_REV=e634aa8789fa8196446ce1d68b459864b93a8780 # Update this if you want to generate off updated snapchain protos
+#!/usr/bin/env bash
+set -euo pipefail
 
-TMPDIR=tmp-protogen
-git clone $PROTO_REPO $TMPDIR
-cd $TMPDIR 
-git checkout $PROTO_REV
+PROTO_REPO="https://github.com/farcasterxyz/snapchain"
+PROTO_PATH="src/proto"
+PROTO_REV="e634aa8789fa8196446ce1d68b459864b93a8780" # Update if you want to regenerate with newer protos
+
+TMPDIR="tmp-protogen"
+
+# Clean up any previous temp dir
+rm -rf "$TMPDIR"
+git clone --depth=1 "$PROTO_REPO" "$TMPDIR"
+cd "$TMPDIR"
+git checkout "$PROTO_REV"
 cd ..
 
-
-# Determine which files you care about
-if [[ "$LIBRARY" == "core" ]]; then
+# Determine which files to generate
+case "${LIBRARY:-}" in
+  core)
     RELEVANT_PROTOS="/defs/request_response.proto /defs/rpc.proto /defs/admin_rpc.proto /defs/message.proto /defs/hub_event.proto /defs/onchain_event.proto /defs/username_proof.proto"
-    OUT_PATH=src/protobufs/generated
+    OUT_PATH="src/protobufs/generated"
     CUSTOM_TS_PROTO_OPTS="outputServices=false"
-elif [[ "$LIBRARY" == "hub-nodejs" ]]; then
+    ;;
+  hub-nodejs)
     RELEVANT_PROTOS="/defs/rpc.proto /defs/admin_rpc.proto"
-    OUT_PATH=src/generated
+    OUT_PATH="src/generated"
     CUSTOM_TS_PROTO_OPTS="outputServices=grpc-js"
-elif [[ "$LIBRARY" == "hub-web" ]]; then
+    ;;
+  hub-web)
     RELEVANT_PROTOS="/defs/rpc.proto /defs/admin_rpc.proto"
-    OUT_PATH=src/generated
+    OUT_PATH="src/generated"
     CUSTOM_TS_PROTO_OPTS="outputClientImpl=grpc-web,lowerCaseServiceMethods=true"
-fi
+    ;;
+  *)
+    echo "Error: LIBRARY environment variable must be set to one of: core, hub-nodejs, hub-web"
+    exit 1
+    ;;
+esac
 
 echo "Generating relevant protos: $RELEVANT_PROTOS"
 echo "Outputting generated files to: $OUT_PATH"
 
-docker run --rm --user $(id -u):$(id -g) -v $(pwd)/../../node_modules:/node_modules -v $(pwd)/$TMPDIR/$PROTO_PATH:/defs -v $(pwd)/$OUT_PATH:/out namely/protoc:1.50_1 --plugin=/node_modules/ts-proto/protoc-gen-ts_proto --ts_proto_out=/out --ts_proto_opt=esModuleInterop=true,exportCommonSymbols=false,useOptionals=none,unrecognizedEnum=false,removeEnumPrefix=true,$CUSTOM_TS_PROTO_OPTS --proto_path=/defs $RELEVANT_PROTOS 
+docker run --rm \
+  --user "$(id -u):$(id -g)" \
+  -v "$(pwd)/../../node_modules":/node_modules \
+  -v "$(pwd)/$TMPDIR/$PROTO_PATH":/defs \
+  -v "$(pwd)/$OUT_PATH":/out \
+  namely/protoc:1.50_1 \
+  --plugin=/node_modules/ts-proto/protoc-gen-ts_proto \
+  --ts_proto_out=/out \
+  --ts_proto_opt=esModuleInterop=true,exportCommonSymbols=false,useOptionals=none,unrecognizedEnum=false,removeEnumPrefix=true,"$CUSTOM_TS_PROTO_OPTS" \
+  --proto_path=/defs $RELEVANT_PROTOS
 
-rm -rf $TMPDIR
+# Clean up
+rm -rf "$TMPDIR"
+
+echo "✅ Protobuf generation complete."
